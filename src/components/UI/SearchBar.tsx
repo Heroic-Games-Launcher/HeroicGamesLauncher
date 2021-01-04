42330ac2b201346c0469839ea53116ebf554d6fb
Flavio Lima <flavioislima@gmail.com>
2021-01-04 19:29:27 +0100

    feat: changed search bar style

--- src/components/UI/SearchBar.tsx
+++ src/components/UI/SearchBar.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from "react";
 interface Props {
-  handleSearch: (input: string) => void
+  handleSearch: (input: string) => void;
 }
 
-export default function SearchBar({handleSearch}: Props) {
+export default function SearchBar({ handleSearch }: Props) {
   return (
     <div className="SearchBar">
-      <span>
-        <input 
-          className="searchInput" 
-          onChange={(event) => handleSearch(event.target.value)} 
-          placeholder={'Enter the game name here...'}
-        />
-          <button className="searchButton">Search Games</button>
-        </span>
+      <span className="material-icons">search</span>
+      <input
+        className="searchInput"
+        onChange={(event) => handleSearch(event.target.value)}
+        placeholder={"Enter the game name here..."}
+      />
     </div>
-  )
+  );
 }
