58d28bb08ff96a1f56b8e4ae59bfa257b7863331
Flavio Lima <flavioislima@gmail.com>
2021-01-04 20:39:35 +0100

    feat: added Settings (WIP)

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import Header from './UI/Header'
+import React from "react";
+import Header from "./UI/Header";
 
 export default function Settings() {
   return (
-    <Header
-      title={'Settings'} 
-      renderBackButton={false}
-    />
-  )
+    <>
+      <Header title={"Settings"} renderBackButton={false} />
+      <div className="Settings">
+        <div className="settingsWrapper">
+          <span className="defaultWine">
+            <span className="settingsText">Default Wine Version</span>
+            <select name="wineVersion" id="wineVersion">
+              <option>Wine Staging</option>
+            </select>
+          </span>
+          <span className="defaultWinePrefix">
+            <span className="settingsText">Default WinePrefix</span>
+            <span>
+              <input
+                type="text"
+                placeholder={"~/.wine"}
+                className="searchInput settingsInput"
+              />
+              <button className="loginButton settingsButton">Choose</button>
+            </span>
+          </span>
+        </div>
+      </div>
+    </>
+  );
 }
