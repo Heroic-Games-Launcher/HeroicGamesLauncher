28f60efe771142bc3301a4eec321c57592cfa0b3
Flavio Lima <flavioislima@gmail.com>
2021-01-07 14:03:11 +0100

    feat: re-added links to gamePage

--- src/components/UI/GameConfig.tsx
+++ src/components/UI/GameConfig.tsx
@@ -11,22 +11,26 @@ import Header from "./Header";
 import "../../App.css";
 import { Game } from '../../types';
 import ContextProvider from '../../state/ContextProvider';
-const { ipcRenderer } = window.require('electron')
+const { ipcRenderer, remote } = window.require('electron');
+const {dialog: { showOpenDialog }} = remote
 
 interface Card {
   location: any;
 }
 
+// This component is becoming really complex and it needs to be refactored in smaller ones
+
 export default function GameConfig({ location }: Card) {
   const [gameInfo, setGameInfo] = useState({} as any);
   const [playing, setPlaying] = useState(false);
-  const [progress, setProgress] = useState('0')
+  const [progress, setProgress] = useState('0.00')
+  const [uninstalling, setUninstalling] = useState(false)
 
   const { handleInstalling, refresh, installing } = React.useContext(ContextProvider)
 
   const { appName } = location.state || {};
   const currentApp = installing.filter(game => game === appName)
-  const isInstalling = currentApp.length;
+  const isInstalling = Boolean(currentApp.length);
 
   useEffect(() => {
     const updateConfig = async () => {
@@ -34,7 +38,7 @@ export default function GameConfig({ location }: Card) {
       setGameInfo(newInfo);
     }
     updateConfig()
-  }, [isInstalling, appName]);
+  }, [isInstalling, appName, uninstalling]);
 
   useEffect(() => {
     const progressInterval = setInterval(() => {
@@ -44,7 +48,7 @@ export default function GameConfig({ location }: Card) {
       }
     }, 2000)
     return () => clearInterval(progressInterval)
-  }, [isInstalling, appName, progress])
+  }, [isInstalling, appName])
 
   if (!appName) {
     return (
@@ -77,6 +81,12 @@ export default function GameConfig({ location }: Card) {
       <>
         <Header renderBackButton />
         <div className="gameConfigContainer">
+          <span className="material-icons is-secondary dots">more_vertical</span>
+        <div className="more">
+          <span className="hidden linkTitle">External Links</span>
+          <span onClick={() => createNewWindow(formatStoreUrl(title))} className="hidden link">Epic Games</span>
+          <span onClick={() => createNewWindow(protonDBurl)} className="hidden link">Check Compatibility</span>
+        </div>
         <div className="gameConfig">
           <img alt="cover-art" src={art_square} className="gameImg" />
           <div className="gameInfo">
@@ -88,75 +98,91 @@ export default function GameConfig({ location }: Card) {
                   <div>Executable: {executable}</div>
                   <div>Size: {sizeInMB}MB</div>
                   <div>Version: {version}</div>
-                  <div className="clickable" onClick={() => ipcRenderer.send('openFolder', install_path)} >Location: {install_path} (Click to Open Location)</div>
+                  <div 
+                    className="clickable" 
+                    onClick={() => ipcRenderer.send('openFolder', install_path)} >Location: {install_path} (Click to Open Location)
+                  </div>
                   <br />
                 </>
               )}
+            </div>
+            <div className="gameStatus">
+              {isInstalling && <progress className="installProgress" max={100} value={progress} />}
+              <p style={ {fontStyle: 'italic', color: (isInstalled || isInstalling) ? '#0BD58C' : '#BD0A0A'}} >{
+                isInstalling ? `Installing ${progress}%` : isInstalled ? "Installed" : "This game is not installed"
+              }</p>
             </div>
             <div className="buttonsWrapper">
               {isInstalled && (
                 <>
                   <div
-                    onClick={async () => {
-                      if (playing) {
-                        return sendKill(appName)
-                      }
-
-                      setPlaying(true);
-                      await legendary(`launch ${appName}`);
-                      setPlaying(false);
-                    }}
-                    className="button is-primary"
+                    onClick={handlePlay()}
+                    className="button is-success"
                   >
                     {playing ? "Playing (Stop)" : "Play Now"}
                   </div>
                 </>
               )}
               <div
-                onClick={async () => {
-                  if (isInstalling) {
-                    handleInstalling(appName)
-                    return sendKill(appName)
-                  }
-
-                  if (isInstalled){
-                    return await legendary(`uninstall ${appName}`)
-                  }
-                  
-                  handleInstalling(appName)                  
-                  return await install(appName)
-                }}
+                onClick={handleInstall(isInstalled)}
                 className={`button ${
-                  isInstalled ? "is-danger" : "is-success"
+                  isInstalled ? "is-danger" : isInstalling ? "is-danger" : "is-primary"
                 }`}
               >
                 {`${
                   isInstalled
                     ? "Uninstall"
                     : isInstalling
-                    ? `${progress}% (Stop)`
+                    ? `Cancel`
                     : "Install"
                 }`}
               </div>
-              {/* <div
-                onClick={() => createNewWindow(formatStoreUrl(title))}
-                className="button is-empty"
-              >
-                Epic Store
-              </div>
-              <div
-                onClick={() => createNewWindow(protonDBurl)}
-                className="button is-empty"
-              >
-                ProtonDB
-              </div> */}
             </div>
           </div>
         </div>
         </div>
-
       </>
     );
   }
   return null;
+
+  function handlePlay(): ((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void) | undefined {
+    return async () => {
+      if (playing) {
+        return sendKill(appName);
+      }
+
+      setPlaying(true);
+      await legendary(`launch ${appName}`);
+      setPlaying(false);
+    };
+  }
+
+  function handleInstall(isInstalled: boolean): ((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void) | undefined {
+    return async () => {
+      if (isInstalling) {
+        handleInstalling(appName);
+        return sendKill(appName);
+      }
+
+      if (isInstalled) {
+        setUninstalling(true);
+        await legendary(`uninstall ${appName}`);
+        return setUninstalling(false);
+      }
+
+      const { filePaths } = await showOpenDialog({
+        title: "Choose Install Path",
+        buttonLabel: "Choose",
+        properties: ["openDirectory"],
+      });
+
+      if (filePaths[0]) {
+        const path = filePaths[0];
+        handleInstalling(appName);
+        await install({ appName, path });
+        handleInstalling(appName);
+      }
+    };
+  }
 }
