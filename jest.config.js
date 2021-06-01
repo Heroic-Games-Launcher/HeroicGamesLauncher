bef5bce9432c0d5d47a26474b03776b800127545
Adityarup Laha <30696515+adityaruplaha@users.noreply.github.com>
2021-06-01 17:55:37 +0530

    Update all packages. (#399)
    Important changes:
    * Moving to Husky v6.
    * Electron 13 (Node 14.7). :tada:
    * Added `@testing-library/dom` as a dev dependency as Yarn was complaining about it.
    * Moved the lint plugins to `devDependencies`.

--- jest.config.js
+++ jest.config.js
@@ -1,50 +1,50 @@
 module.exports = {
-  // The root of your source code, typically /src
-  // `<rootDir>` is a token Jest substitutes
-  roots: ["<rootDir>"],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.config.js'
+  ],
+
+  coverageDirectory: '<rootDir>/coverage',
+
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules>',
+    '<rootDir>/public',
+    '<rootDir>/electron',
+    '<rootDir>/dist',
+    '<rootDir>/build',
+    '<rootDir>/coverage'
+  ],
+
+  coverageReporters: ['text', 'html'],
 
   moduleDirectories: [
-    "node_modules",
-    "<rootDir>"
-    ],
+    'node_modules',
+    '<rootDir>'
+  ],
 
-  // Jest transformations -- this adds support for TypeScript
-  // using ts-jest
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
+  // Module file extensions for importing
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 
   moduleNameMapper: {
-    "\\.css$": "<rootDir>/src/test_helpers/mock/css.ts",
-    "electron": "<rootDir>/src/test_helpers/mock/electron.ts"
+    '\\.css$': '<rootDir>/src/test_helpers/mock/css.ts',
+    'electron': '<rootDir>/src/test_helpers/mock/electron.ts'
   },
 
   resetMocks: true,
 
-  setupFilesAfterEnv: ["<rootDir>/src/test_helpers/setupTests.ts"],
+  // The root of your source code, typically /src
+  // `<rootDir>` is a token Jest substitutes
+  roots: ['<rootDir>'],
+
+  setupFilesAfterEnv: ['<rootDir>/src/test_helpers/setupTests.ts'],
 
   // Test spec file resolution pattern
   // should contain `test` or `spec`.
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
 
-  // Module file extensions for importing
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-
-  coverageDirectory: "<rootDir>/coverage",
-
-  collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}",
-    "!**/*.config.js"
-  ],
-
-  coveragePathIgnorePatterns: [
-    "<rootDir>/node_modules>",
-    "<rootDir>/public",
-    "<rootDir>/electron",
-    "<rootDir>/dist",
-    "<rootDir>/build",
-    "<rootDir>/coverage"
-  ],
-
-  coverageReporters: ['text', 'html']
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  }
 };
