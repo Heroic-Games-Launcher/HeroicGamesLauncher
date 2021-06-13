6ca14a23df2cc8fee201b34e5f59f576a9291e6e
dragonDScript <agomila96@gmail.com>
2021-06-13 20:50:46 +0200

    Add docs

--- electron/logger.ts
+++ electron/logger.ts
@@ -1,3 +1,10 @@
+/**
+ * Listens to stdout and stderr and saves the messages into the RAM.
+ * When the process is about to halt, it resolves the promise and lets you
+ * do anything with the logs. In the current implementation, it saves the logs to a
+ * file in .config/heroic/Crash Dumps, with this format: {current local time}.txt
+ * @returns Promise<string[]>
+ */
 export function listenStdout(): Promise<string[]> {
   const promise: Promise<string[]> = new Promise((resolve, reject) => {
     const stdout = process.stdout;
