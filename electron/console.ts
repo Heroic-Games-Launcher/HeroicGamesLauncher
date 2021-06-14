24d9e725224fc6fca6495b58101083cd8d286593
Niklas <61798668+Nocccer@users.noreply.github.com>
2021-06-14 15:47:04 +0200

    Added test for console.ts (#1)
    * Added test for console.ts
    
    * Fix test description

--- electron/console.ts
+++ electron/console.ts
@@ -1,22 +1,38 @@
 /**
- * Use this module to log things into the console.
- * Everything will be saved to a file before the app exits.
- * Note that with console.log and console.warn everything will be saved too.
- * error equals console.error
+ * @file  Use this module to log things into the console.
+ *        Everything will be saved to a file before the app exits.
+ *        Note that with console.log and console.warn everything will be saved too.
+ *        error equals console.error
  */
 
-
+/**
+ * Log debug messages
+ * @param text debug messages to log
+ */
 export function debug(...text: string[]) {
   console.log(`DEBUG: ${text.join(' ')}`)
 }
 
+/**
+ * Log error messages
+ * @param text error messages to log
+ */
+export function error(...text: string[]) {
+  console.error(`ERROR: ${text.join(' ')}`);
+}
 
-export const error = console.error;
-
+/**
+ * Log info messages
+ * @param text info messages to log
+ */
 export function info(...text: string[]) {
   console.log(`INFO: ${text.join(' ')}`)
 }
 
+/**
+ * Log warning messages
+ * @param text warning messages to log
+ */
 export function warning(...text: string[]) {
   console.log(`WARNING: ${text.join(' ')}`)
 }
