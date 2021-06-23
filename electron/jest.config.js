8ac3a6fa4ca84c80013bb9cdd216ecfe7379843b
Niklas <61798668+Nocccer@users.noreply.github.com>
2021-06-23 17:26:05 +0200

    Fixed electron-builder config (#468)
    * Fixed electron-builder config
    
    * Adapt electron-builder
    
    * Fix missing jest split

--- electron/jest.config.js
+++ electron/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  displayName: 'Frontend',
+  displayName: 'Backend',
 
   globals: {
     'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json'
+      tsconfig: '<rootDir>/electron/tsconfig.json'
     }
   },
 
@@ -11,15 +11,8 @@ module.exports = {
     'node_modules',
     '<rootDir>'
   ],
-
   // Module file extensions for importing
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-
-  moduleNameMapper: {
-    '\\.css$': '<rootDir>/src/test_helpers/mock/css.ts',
-    'electron': '<rootDir>/src/test_helpers/mock/electron.ts',
-    'react-i18next': '<rootDir>/src/test_helpers/mock/react-i18next.ts'
-  },
 
   resetMocks: true,
 
@@ -27,17 +20,15 @@ module.exports = {
 
   // The root of your source code, typically /src
   // `<rootDir>` is a token Jest substitutes
-  roots: ['<rootDir>/src'],
-
-  setupFilesAfterEnv: ['<rootDir>/src/test_helpers/setupTests.ts'],
+  roots: ['<rootDir>/electron'],
 
   // Test spec file resolution pattern
   // should contain `test` or `spec`.
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-
   // Jest transformations -- this adds support for TypeScript
   // using ts-jest
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   }
 };
+
