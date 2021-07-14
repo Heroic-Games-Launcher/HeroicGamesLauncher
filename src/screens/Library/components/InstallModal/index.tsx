cd686487a3a2d5c803b5254dbbc12ce6ec5f9134
Flávio F Lima <flavioislima@gmail.com>
2021-07-14 16:18:15 +0200

    Show Modal with Install Options when installing from Game Card (#540)
    * feat: added InstallModal component
    
    * feat: simplified UI and other fixes
    
    * chore: more UI fixes
    
    * chore: removed log
    
    * fix: tests

---
+++ src/screens/Library/components/InstallModal/index.tsx
@@ -0,0 +1,72 @@
+import { getGameInfo, install } from 'src/helpers'
+import React, { useContext, useEffect, useState } from 'react'
+
+import './index.css'
+import { InstallProgress } from 'src/types'
+import { NOT_SUPPORTED_GAMES } from 'src/constants'
+
+import { UpdateComponent } from 'src/components/UI'
+import { useTranslation } from 'react-i18next'
+import ContextProvider from 'src/state/ContextProvider'
+
+type Props = {
+  appName: string
+  backdropClick: () => void
+}
+
+const storage: Storage = window.localStorage
+
+export default function InstallModal({appName, backdropClick}: Props) {
+  const notSupported = NOT_SUPPORTED_GAMES.includes(appName)
+  const {handleGameStatus} = useContext(ContextProvider)
+  const [title, setTitle] = useState('')
+
+  const {t} = useTranslation('gamepage')
+  const previousProgress = JSON.parse(storage.getItem(appName) || '{}') as InstallProgress
+
+  function handleInstall(path: string){
+    install({
+      appName,
+      handleGameStatus,
+      installPath: path,
+      isInstalling: false,
+      previousProgress,
+      progress: previousProgress,
+      t
+    })
+    backdropClick()
+  }
+
+  useEffect(() => {
+    const getInfo = async() => {
+      const {title} = await getGameInfo(appName)
+      setTitle(title)
+    }
+    getInfo()
+  }, [appName])
+
+  return (
+    <span className="modalContainer">
+      {title ?
+        <div className="modal">
+          <span className="title">{title}</span>
+          <div className="buttonsContainer">
+            {!notSupported && <button
+              onClick={() => handleInstall('another')}
+              className={`button is-primary`}
+            >
+              Install
+            </button>}
+            <button
+              onClick={() => handleInstall('import')}
+              className={`button is-tertiary`}
+            >
+              Import
+            </button>
+          </div>
+        </div>
+        : <UpdateComponent />}
+      <span className="backdrop" onClick={() => backdropClick()} />
+    </span>
+  )
+}
