2332b06a3c15027072d28d617f756c7c8c91420b
Raul Dipeas <rauldipeas@mail.com>
2021-08-02 18:56:12 -0300

    ^madrepo

--- madrepo.sh
+++ madrepo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 echo 'deb Https://sourceforge.net/projects/madlinux/files/repo core main'|sudo tee /etc/apt/sources.list.d/madlinux.list
 wget -qO- Https://sourceforge.net/projects/madlinux/files/repo/madlinux.key|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/madlinux.gpg>/dev/null
 sudo add-apt-repository -y ppa:apt-fast/stable
