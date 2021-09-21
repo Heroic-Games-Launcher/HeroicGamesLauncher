0c1aac9e61bb9255090b8ea651dcc175b4a6198c
Nocccer <Nocccer@gmail.com>
2021-09-21 17:00:29 +0200

    Adaptions
    - removed fetchData, because not really needed
    - adapt fetchReleases to fetch more details about releases

--- electron/wineup/__tests__/api.test.ts
+++ electron/wineup/__tests__/api.test.ts
@@ -1,15 +1,15 @@
-import { fetchReleases, fetchData } from '../api';
-import { test_data_release_list, test_data_release } from './github-api-test-data.json';
+import { fetchReleases } from '../api';
+import { test_data_release_list } from './github-api-test-data.json';
 import * as axios from 'axios'
 
-describe('api', () => {
+describe('api- fetchReleases', () => {
   test('fetch releases succesfully', async () => {
     axios.default.get = jest.fn().mockResolvedValue(test_data_release_list);
     const releases = await fetchReleases();
     expect(axios.default.get).toBeCalledWith('https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases?per_page=100');
     expect(releases).not.toBe([]);
     expect(releases.length).toBeGreaterThan(0);
-    expect(releases).toContain('6.16-GE-1');
+    expect(releases[2].version).toContain('6.16-GE-1');
   })
 
   test('fetch releases failed because of 404', async () => {
@@ -19,61 +19,6 @@ describe('api', () => {
     expect(axios.default.get).toBeCalledWith('https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases?per_page=100');
     expect(releases).toBe(undefined);
     expect(console.error).toBeCalledWith('ERROR: Could not fetch available wine-ge versions.');
-    expect(console.error).toBeCalledWith('ERROR: Could not fetch tag 404');
-  })
-
-  test('fetch data succesfully with given tag', async () => {
-    axios.default.get = jest.fn().mockResolvedValue(test_data_release);
-    const data = await fetchData('6.16-GE-1');
-    expect(axios.default.get).toBeCalledWith('https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases/tags/6.16-GE-1');
-    expect(data.version).toBe('6.16-GE-1');
-    expect(data.checksum).toBe(
-      'https://github.com/GloriousEggroll/wine-ge-custom/releases/download/6.16-GE-1/lutris-ge-6.16-1-x86_64.sha512sum');
-    expect(data.download).toBe(
-      'https://github.com/GloriousEggroll/wine-ge-custom/releases/download/6.16-GE-1/lutris-ge-6.16-1-x86_64.tar.xz');
-    expect(data.date).toBe('2021-08-29');
-    expect(data.size).toBe(89977056);
-  })
-
-  test('fetch data failed because of wrong tag', async () => {
-    axios.default.get = jest.fn().mockResolvedValue(test_data_release);
-    console.error = jest.fn();
-    const data = await fetchData('invalid');
-    expect(axios.default.get).toBeCalledWith('https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases/tags/invalid');
-    expect(data).toBe(undefined);
-    expect(console.error).toBeCalledWith('ERROR: Could not fetch given tag (invalid) of wine-ge');
-  })
-
-  test('fetch data failed because of 404 for given tag', async () => {
-    axios.default.get = jest.fn().mockRejectedValue('Could not fetch tag 404');
-    console.error = jest.fn();
-    const data = await fetchData('invalid');
-    expect(axios.default.get).toBeCalledWith('https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases/tags/invalid');
-    expect(data).toBe(undefined);
-    expect(console.error).toBeCalledWith('ERROR: Could not fetch given tag (invalid) of wine-ge');
-    expect(console.error).toBeCalledWith('ERROR: Could not fetch tag 404');
-  })
-
-  test('fetch data latest succesfully if none given', async () => {
-    axios.default.get = jest.fn().mockResolvedValue(test_data_release);
-    const data = await fetchData();
-    expect(axios.default.get).toBeCalledWith('https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases/latest');
-    expect(data.version).toBe('6.16-GE-1');
-    expect(data.checksum).toBe(
-      'https://github.com/GloriousEggroll/wine-ge-custom/releases/download/6.16-GE-1/lutris-ge-6.16-1-x86_64.sha512sum');
-    expect(data.download).toBe(
-      'https://github.com/GloriousEggroll/wine-ge-custom/releases/download/6.16-GE-1/lutris-ge-6.16-1-x86_64.tar.xz');
-    expect(data.date).toBe('2021-08-29');
-    expect(data.size).toBe(89977056);
-  })
-
-  test('fetch data latest failed because of 404', async () => {
-    axios.default.get = jest.fn().mockRejectedValue('Could not fetch tag 404');
-    console.error = jest.fn();
-    const data = await fetchData();
-    expect(axios.default.get).toBeCalledWith('https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases/latest');
-    expect(data).toBe(undefined);
-    expect(console.error).toBeCalledWith('ERROR: Could not fetch latest wine-ge');
     expect(console.error).toBeCalledWith('ERROR: Could not fetch tag 404');
   })
 })
--- electron/wineup/api.ts
+++ electron/wineup/api.ts
@@ -5,7 +5,7 @@
 
 import * as axios from 'axios'
 import { WINEGE_URL } from './constants';
-import { logError, logInfo } from '../logger';
+import { logError } from '../logger';
 
 /**
  * Interface contains information about a release
@@ -24,59 +24,30 @@ interface ReleaseData {
 }
 
 /**
- * Fetches data (@see ReleaseData) of latest or given release.
- * @param tag   release to fetch data for
- * @returns     config of @see ReleaseData
- */
-async function fetchData(tag?: string): Promise<ReleaseData> {
-  const release_data = {} as ReleaseData;
-
-  const url = WINEGE_URL + (tag ? `/tags/${tag}` : '/latest');
-  try {
-    logInfo(`Fetch wine-ge tag from ${url}`);
-    const data = await axios.default.get(url);
-
-    if ((tag && data.data.tag_name !== tag) || !data.data.tag_name) {
-      tag
-        ? logError(`Could not fetch given tag (${tag}) of wine-ge`)
-        : logError(`Could not fetch latest wine-ge`)
-      return;
-    }
-
-    release_data.version = data.data.tag_name;
-    release_data.date = data.data.published_at.split('T')[0];
-
-    for (const asset of data.data.assets) {
-      if (asset.name.endsWith('sha512sum')) {
-        release_data.checksum = asset.browser_download_url;
-      }
-      else if (asset.name.endsWith('tar.gz') || asset.name.endsWith('tar.xz')) {
-        release_data.download = asset.browser_download_url;
-        release_data.size = asset.size;
-      }
-    }
-    return release_data;
-  }
-  catch (error) {
-    logError(error);
-    tag
-      ? logError(`Could not fetch given tag (${tag}) of wine-ge`)
-      : logError('Could not fetch latest wine-ge')
-    return;
-  }
-}
-
-/**
  * Fetches all available wine-ge releases.
  * @param count max releases to fetch for (default: 100)
- * @returns string list of available releases
+ * @returns ReleaseData list of available releases
  */
-async function fetchReleases(count = '100'): Promise<string[]> {
-  const releases = [];
+async function fetchReleases(count = '100'): Promise<ReleaseData[]> {
+  const releases :ReleaseData[] = [];
   try {
     const data = await axios.default.get(WINEGE_URL + '?per_page=' + count)
     for (const release of data.data) {
-      releases.push(release.tag_name);
+      const release_data = {} as ReleaseData;
+      release_data.version = release.tag_name;
+      release_data.date = release.published_at.split('T')[0];
+
+      for (const asset of release.assets) {
+        if (asset.name.endsWith('sha512sum')) {
+          release_data.checksum = asset.browser_download_url;
+        }
+        else if (asset.name.endsWith('tar.gz') || asset.name.endsWith('tar.xz')) {
+          release_data.download = asset.browser_download_url;
+          release_data.size = asset.size;
+        }
+      }
+
+      releases.push(release_data);
     }
   }
   catch (error) {
@@ -87,7 +58,163 @@ async function fetchReleases(count = '100'): Promise<string[]> {
   return releases;
 }
 
+// PROTOTYPE OF DOWNLOAD FUNCTION
+
+// async function downloadWineGE(releases: ReleaseData[], installDir: string, version?: string): Promise<ReleaseData>
+// {
+//   let release_to_download = undefined;
+
+//   // Check if installDir exist
+//   if(!fs.existsSync(installDir))
+//   {
+//     logError(`Installation directory ${installDir} doesn't exist!`);
+//     return undefined;
+//   }
+
+//   // check if provided version is avaiable
+//   if(version)
+//   {
+//     for (const release of releases)
+//     {
+//       if(release.version === version)
+//       {
+//         release_to_download = release;
+//         break;
+//       }
+//     }
+
+//     if(!release_to_download)
+//     {
+//       logError(`Given version (${version}) is not available to download!`);
+//       return undefined;
+//     }
+//   }
+//   else
+//   {
+//     release_to_download = releases[0]; // latest release by default
+//   }
+
+//   if (!release_to_download.download)
+//   {
+//     logError(`Didn't find binary for ${release_to_download.version}.`)
+//     return undefined;
+//   }
+
+//   const winedir = installDir + '/Wine-' + release_to_download.version;
+//   const checksum_dir = winedir + '/sha512sum'
+//   // Fixme: axios not fetching the content of the page as text here
+//   // the later checksum check fails
+//   const source_checksum = release_to_download.checksum
+//     ? (await axios.default.get(release_to_download.checksum)).data
+//     : undefined
+//   const local_checksum = fs.existsSync(checksum_dir)
+//     ? fs.readFileSync(checksum_dir).toString()
+//     : undefined
+
+//   // Check if it already exist
+//   if (fs.existsSync(winedir)) {
+//     if (local_checksum && source_checksum) {
+//       if (source_checksum.includes(local_checksum))
+//       {
+//         logInfo(`Proton-${release_to_download.version} already installed`);
+//         logInfo(`No hotfix found`);
+//         return;
+//       }
+//       logInfo('Hotfix available');
+//     }
+//     else
+//     {
+//       logInfo(`Proton-${release_to_download.version} already installed`);
+//       return;
+//     }
+//   }
+
+//   // Prepare destination where to download tar file
+//   const tar_file = installDir + release_to_download.download.split('/').slice(-1)[0];
+
+//   if(fs.existsSync(tar_file))
+//   {
+//     try {
+//       fs.unlinkSync(tar_file);
+//     } catch(error) {
+//       logError(error);
+//       logError(`Failed to remove already existing ${tar_file}!`);
+//       return undefined;
+//     }
+//   }
+
+//   // Download
+//   try {
+//     await execAsync(`curl -L -o ${tar_file} ${release_to_download.download}`);
+//   }
+//   catch (error)
+//   {
+//     logError(error);
+//     logError(`Download of Wine-${release_to_download.version} failed!`);
+//   }
+
+//   // Check if download checksum is correct
+//   const fileBuffer = fs.readFileSync(tar_file);
+//   const hashSum = crypto.createHash('sha256');
+//   hashSum.update(fileBuffer);
+
+//   const download_checksum = hashSum.digest('hex');
+//   logInfo(download_checksum);
+//   logInfo(source_checksum);
+//   if (!source_checksum.includes(download_checksum))
+//   {
+//     logError('Checksum verification failed');
+//     try {
+//       fs.unlinkSync(tar_file);
+//     } catch(error) {
+//       logError(error);
+//     }
+//     return undefined;
+//   }
+
+//   // Unzip
+//   if (fs.existsSync(winedir))
+//   {
+//     try {
+//       fs.rmdirSync(winedir, { recursive: true });
+//     } catch(error) {
+//       logError(error);
+//       logError(`Failed to remove already existing folder ${winedir}!`);
+//       return undefined;
+//     }
+//   }
+
+//   try {
+//     if(tar_file.endsWith('tar.gz'))
+//     {
+//       await execAsync(`tar -zxf ${tar_file} --directory ${installDir}`);
+//     }
+//     else if(tar_file.endsWith('tar.xz'))
+//     {
+//       await execAsync(`tar -jxf ${tar_file} --directory ${installDir}`);
+//     }
+//     logInfo(`Unzip ${tar_file.split('/').slice(-1)[0]} succesfully to ${installDir}`);
+//   }
+//   catch (error)
+//   {
+//     logError(error);
+//     logError(`Unzip of ${tar_file.split('/').slice(-1)[0]} failed!`);
+//     return undefined;
+//   }
+
+//   // Clean up
+//   try {
+//     fs.unlinkSync(tar_file);
+//   } catch(error) {
+//     logError(error);
+//     logError(`Failed to remove ${tar_file}!`);
+//   }
+
+//   return release_to_download;
+// }
+
+
 export {
-  fetchData,
+  //downloadWineGE,
   fetchReleases
 }
