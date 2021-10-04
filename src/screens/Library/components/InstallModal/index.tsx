81d336f821db79ecb7daa2caccfa32465b2c1b91
Flavio F Lima <flavioislima@gmail.com>
2021-10-04 19:02:06 +0200

    feat: add SDL on the install modal

--- src/screens/Library/components/InstallModal/index.tsx
+++ src/screens/Library/components/InstallModal/index.tsx
@@ -8,6 +8,8 @@ import { UpdateComponent } from 'src/components/UI'
 import { useTranslation } from 'react-i18next'
 import ContextProvider from 'src/state/ContextProvider'
 
+import {SDL_GAMES, SelectiveDownload} from './selective_dl'
+
 type Props = {
   appName: string
   backdropClick: () => void
@@ -18,6 +20,8 @@ const storage: Storage = window.localStorage
 export default function InstallModal({appName, backdropClick}: Props) {
   const {handleGameStatus} = useContext(ContextProvider)
   const [title, setTitle] = useState('')
+  const [sdlList, setSdlList] = useState([] as Array<string>)
+  const haveSDL = Boolean(SDL_GAMES[appName])
 
   const {t} = useTranslation('gamepage')
   const previousProgress = JSON.parse(storage.getItem(appName) || '{}') as InstallProgress
@@ -31,8 +35,21 @@ export default function InstallModal({appName, backdropClick}: Props) {
       isInstalling: false,
       previousProgress,
       progress: previousProgress,
-      t
+      t,
+      sdlList
     })
+  }
+
+  function handleSdl(tags: Array<string>){
+    let updatedList: Array<string> = [...sdlList]
+    tags.forEach(tag => {
+      if (updatedList.includes(tag)){
+        return updatedList = updatedList.filter((tagx) => {
+          return tagx !== tag})
+      }
+      return updatedList.push(tag)
+    })
+    setSdlList([...updatedList])
   }
 
   useEffect(() => {
@@ -48,18 +65,28 @@ export default function InstallModal({appName, backdropClick}: Props) {
       {title ?
         <div className="modal">
           <span className="title">{title}</span>
+          {haveSDL && <div className="sdlContainer">
+            <p className="sdlTitle" >{t('sdl.title', 'Select components to Install')}</p>
+            {SDL_GAMES[appName].map(({name, tags}: SelectiveDownload) => {
+              return (
+                <span key={name} className="sdlItem">
+                  <span className="sdlName">{name}</span><input type='checkbox' onChange={() => handleSdl(tags)}/>
+                </span>)
+            }
+            )}
+          </div>}
           <div className="buttonsContainer">
             <button
               onClick={() => handleInstall('another')}
               className={`button is-primary`}
             >
-              Install
+              {t('button.install')}
             </button>
             <button
               onClick={() => handleInstall('import')}
               className={`button is-tertiary`}
             >
-              Import
+              {t('button.import')}
             </button>
           </div>
         </div>
