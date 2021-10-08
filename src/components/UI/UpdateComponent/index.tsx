2929a0079658f75cdde1da4b9b29d315b5b524d3
Flavio F Lima <flavioislima@gmail.com>
2021-10-08 17:16:11 +0200

    chore: small UI adjustments

--- src/components/UI/UpdateComponent/index.tsx
+++ src/components/UI/UpdateComponent/index.tsx
@@ -6,7 +6,7 @@ import Update from '@material-ui/icons/Update'
 export default function UpdateComponent() {
   return (
     <div className="updateIcon" data-testid="updateComponent">
-      <Update className="material-icons" />
+      <Update className="material-icons" style={{zIndex: 999}} />
     </div>
   )
 }
