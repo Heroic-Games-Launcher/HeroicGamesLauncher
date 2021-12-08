2d2f35a0683341145187ce766b2c6fd1d24e175d
Flavio F Lima <flavioislima@gmail.com>
2021-12-08 18:15:16 +0100

    feat: add installation path on Install modal + other improvements

--- src/helpers/library.ts
+++ src/helpers/library.ts
@@ -12,34 +12,57 @@ const storage: Storage = window.localStorage
 
 type InstallArgs = {
   appName: string
-  handleGameStatus: (game: GameStatus) =>  Promise<void>
+  handleGameStatus: (game: GameStatus) => Promise<void>
   installPath: string
   isInstalling: boolean
   previousProgress: InstallProgress | null
   progress: InstallProgress
   setInstallPath?: (path: string) => void
-  t: TFunction<'gamepage'>,
-  installDlcs?: boolean,
+  t: TFunction<'gamepage'>
+  installDlcs?: boolean
   sdlList?: Array<string>
 }
 
-async function install({appName, installPath, t, progress, isInstalling, handleGameStatus, previousProgress, setInstallPath, sdlList = [], installDlcs = false}: InstallArgs) {
-  if(!installPath)
-  {
-    return;
+async function install({
+  appName,
+  installPath,
+  t,
+  progress,
+  isInstalling,
+  handleGameStatus,
+  previousProgress,
+  setInstallPath,
+  sdlList = [],
+  installDlcs = false
+}: InstallArgs) {
+  if (!installPath) {
+    return
   }
 
-  const {folder_name, is_game, is_installed}: GameInfo = await getGameInfo(appName)
+  console.trace({ installPath, isInstalling })
+
+  const { folder_name, is_game, is_installed }: GameInfo = await getGameInfo(
+    appName
+  )
   if (isInstalling) {
     if (installPath === 'default') {
-      const { defaultInstallPath }: AppSettings = await ipcRenderer.invoke('requestSettings', 'default')
+      const { defaultInstallPath }: AppSettings = await ipcRenderer.invoke(
+        'requestSettings',
+        'default'
+      )
       installPath = defaultInstallPath
     }
-    return handleStopInstallation(appName, [installPath, folder_name], t, progress)
+    console.log({ installPath })
+    return handleStopInstallation(
+      appName,
+      [installPath, folder_name],
+      t,
+      progress
+    )
   }
 
   if (is_installed) {
-    return uninstall({appName, handleGameStatus, t})
+    return uninstall({ appName, handleGameStatus, t })
   }
 
   if (installPath === 'import' && is_game) {
@@ -59,27 +82,27 @@ async function install({appName, installPath, t, progress, isInstalling, handleG
     return await handleGameStatus({ appName, status: 'done' })
   }
 
-  if (installPath === 'another' || !is_game) {
-    const args = {
-      buttonLabel: t('gamepage:box.choose'),
-      properties: ['openDirectory'],
-      title: t('gamepage:box.installpath')
-    }
-    const { path, canceled } = await ipcRenderer.invoke('openDialog', args)
-
-    if (canceled || !path) {
-      return
-    }
-
-    setInstallPath && setInstallPath(path)
+  if (installPath !== 'default' || !is_game) {
+    console.trace({ installPath, isInstalling })
+    setInstallPath && setInstallPath(installPath)
     // If the user changed the previous folder, the percentage should start from zero again.
-    if (previousProgress && previousProgress.folder !== path) {
+    if (previousProgress && previousProgress.folder !== installPath) {
       storage.removeItem(appName)
     }
-    handleGameStatus({ appName, status: 'installing' })
-    const result = await ipcRenderer.invoke('install', { appName, path: `'${path}'`, installDlcs, sdlList })
+    handleGameStatus({
+      folder: installPath,
+      appName,
+      status: 'installing',
+      progress: previousProgress?.percent || '0%'
+    })
+    const result = await ipcRenderer.invoke('install', {
+      appName,
+      path: `${installPath}`,
+      installDlcs,
+      sdlList
+    })
 
-    if (result && result.status === 'error'){
+    if (result && result.status === 'error') {
       return await handleGameStatus({ appName, status: 'error' })
     }
 
@@ -93,7 +116,10 @@ async function install({appName, installPath, t, progress, isInstalling, handleG
     // If the user changed the previous folder, the percentage should start from zero again.
     let path = installPath
     if (installPath === 'default') {
-      const {defaultInstallPath}: AppSettings = await ipcRenderer.invoke('requestSettings', 'default')
+      const { defaultInstallPath }: AppSettings = await ipcRenderer.invoke(
+        'requestSettings',
+        'default'
+      )
       path = defaultInstallPath
     }
     if (previousProgress && previousProgress.folder !== path) {
@@ -102,9 +128,14 @@ async function install({appName, installPath, t, progress, isInstalling, handleG
 
     await handleGameStatus({ appName, status: 'installing' })
 
-    const result = await ipcRenderer.invoke('install', { appName, path: `'${path}'`, installDlcs, sdlList })
+    const result = await ipcRenderer.invoke('install', {
+      appName,
+      path: `'${path}'`,
+      installDlcs,
+      sdlList
+    })
 
-    if (result && result.status === 'error'){
+    if (result && result.status === 'error') {
       return await handleGameStatus({ appName, status: 'error' })
     }
 
@@ -114,7 +145,6 @@ async function install({appName, installPath, t, progress, isInstalling, handleG
 
     return await handleGameStatus({ appName, status: 'done' })
   }
-
 }
 
 const importGame = async (args: {
@@ -124,11 +154,11 @@ const importGame = async (args: {
 
 type UninstallArgs = {
   appName: string
-  handleGameStatus: (game: GameStatus) =>  Promise<void>
+  handleGameStatus: (game: GameStatus) => Promise<void>
   t: TFunction<'gamepage'>
 }
 
-async function uninstall({appName, handleGameStatus, t}: UninstallArgs) {
+async function uninstall({ appName, handleGameStatus, t }: UninstallArgs) {
   const args = {
     buttons: [t('box.yes'), t('box.no')],
     message: t('gamepage:box.uninstall.message'),
@@ -153,7 +183,6 @@ async function handleStopInstallation(
   t: TFunction<'gamepage'>,
   progress: InstallProgress
 ) {
-
   const args = {
     buttons: [
       t('gamepage:box.stopInstall.keepInstalling'),
@@ -167,7 +196,7 @@ async function handleStopInstallation(
   const { response } = await ipcRenderer.invoke('openMessageBox', args)
 
   if (response === 1) {
-    storage.setItem(appName, JSON.stringify({...progress, folder: path}))
+    storage.setItem(appName, JSON.stringify({ ...progress, folder: path }))
     return sendKill(appName)
   } else if (response === 2) {
     await sendKill(appName)
@@ -180,11 +209,20 @@ const repair = async (appName: string): Promise<void> =>
   await ipcRenderer.invoke('repair', appName)
 
 type LaunchOptions = {
-  appName: string, t: TFunction<'gamepage'>, handleGameStatus: (game: GameStatus) => Promise<void>, launchArguments?: string
+  appName: string
+  t: TFunction<'gamepage'>
+  handleGameStatus: (game: GameStatus) => Promise<void>
+  launchArguments?: string
 }
 
-const launch = ({appName, handleGameStatus,t, launchArguments}: LaunchOptions): Promise<void> =>
-  ipcRenderer.invoke('launch', {appName, launchArguments})
+const launch = ({
+  appName,
+  handleGameStatus,
+  t,
+  launchArguments
+}: LaunchOptions): Promise<void> =>
+  ipcRenderer
+    .invoke('launch', { appName, launchArguments })
     .then(async (err: string | string[]) => {
       if (!err) {
         return
@@ -192,7 +230,7 @@ const launch = ({appName, handleGameStatus,t, launchArguments}: LaunchOptions): 
 
       if (
         typeof err === 'string' &&
-      err.includes('ERROR: Game is out of date')
+        err.includes('ERROR: Game is out of date')
       ) {
         const args = {
           buttons: [t('gamepage:box.yes'), t('box.no')],
@@ -209,7 +247,10 @@ const launch = ({appName, handleGameStatus,t, launchArguments}: LaunchOptions): 
           return await handleGameStatus({ appName, status: 'done' })
         }
         await handleGameStatus({ appName, status: 'playing' })
-        await ipcRenderer.invoke('launch', {appName: `${appName} --skip-version-check`, launchArguments})
+        await ipcRenderer.invoke('launch', {
+          appName: `${appName} --skip-version-check`,
+          launchArguments
+        })
         return await handleGameStatus({ appName, status: 'done' })
       }
     })
@@ -220,9 +261,10 @@ const updateGame = (appName: string): Promise<void> =>
 // Todo: Get Back to update all games
 function updateAllGames(
   gameList: Array<string>,
-  handleGameStatus: (game: GameStatus) => Promise<void>) {
+  handleGameStatus: (game: GameStatus) => Promise<void>
+) {
   gameList.forEach(async (appName) => {
-    await handleGameStatus({appName, status: 'updating'})
+    await handleGameStatus({ appName, status: 'updating' })
     await updateGame(appName)
   })
 }
@@ -238,8 +280,9 @@ function getRecentGames(library: GameInfo[]) {
     const configStore: ElectronStore = new Store({
       cwd: 'store'
     })
-    const recentGames: Array<RecentGame> = configStore.get('games.recent') as Array<RecentGame> || []
-    const recentGamesList = recentGames.map(a => a.appName) as string[]
+    const recentGames: Array<RecentGame> =
+      (configStore.get('games.recent') as Array<RecentGame>) || []
+    const recentGamesList = recentGames.map((a) => a.appName) as string[]
     return recentGamesList.includes(game.app_name)
   })
 }
