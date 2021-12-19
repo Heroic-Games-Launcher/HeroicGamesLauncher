3ac91bc7c4b858de48abda4d71c356298142ec2b
Flavio F Lima <flavioislima@gmail.com>
2021-12-19 16:43:30 +0100

    feat: add method to login with returned sid

--- src/screens/WebView/index.tsx
+++ src/screens/WebView/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { useLocation } from 'react-router'
+import ElectronStore from 'electron-store'
+
 import { UpdateComponent } from 'src/components/UI'
 import WebviewControls from 'src/components/UI/WebviewControls'
+import ContextProvider from 'src/state/ContextProvider'
 import { Webview } from 'src/types'
-const { clipboard } = window.require('electron')
+
+const Store = window.require('electron-store')
+const configStore: ElectronStore = new Store({
+  cwd: 'store'
+})
+const { clipboard, ipcRenderer } = window.require('electron')
 import './index.css'
 
 type Props = {
   isLogin?: boolean
+}
+
+type SID = {
+  sid: string
 }
 
 const USER_AGENT =
@@ -17,11 +29,19 @@ const USER_AGENT =
 export default function WebView({ isLogin }: Props) {
   const { i18n } = useTranslation()
   const { pathname } = useLocation()
+  const { t } = useTranslation()
+  const user = configStore.get('userInfo')
+
   let lang = i18n.language
   if (i18n.language === 'pt') {
     lang = 'pt-BR'
   }
-  const [loading, setLoading] = React.useState(false)
+
+  const [loading, setLoading] = React.useState<{
+    refresh: boolean
+    message: string
+  }>({ refresh: false, message: t('loading.website', 'Loading Website') })
+  const { refreshLibrary } = React.useContext(ContextProvider)
 
   const loginUrl =
     'https://www.epicgames.com/id/login?redirectUrl=https%3A%2F%2Fwww.epicgames.com%2Fid%2Fapi%2Fredirect'
@@ -39,16 +59,48 @@ export default function WebView({ isLogin }: Props) {
     '/login': loginUrl
   }
 
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     const webview = document.querySelector('webview') as Webview
     if (webview) {
       const loadstart = () => {
-        setLoading(true)
+        setLoading({ ...loading, refresh: true })
       }
 
       const loadstop = () => {
-        setLoading(false)
-        console.log(webview.getURL())
+        setLoading({ ...loading, refresh: false })
+
+        // Deals with Login
+        if (!user) {
+          webview.findInPage('sid')
+          webview.addEventListener('found-in-page', async (res) => {
+            const data = res as Event & { result: { matches: number } }
+            if (data.result.matches) {
+              webview.focus()
+              webview.selectAll()
+              webview.copy()
+              const { sid }: SID = JSON.parse(clipboard.readText())
+              console.log({ sid })
+              try {
+                setLoading({
+                  refresh: true,
+                  message: t('status.logging', 'Logging In...')
+                })
+                await ipcRenderer.invoke('login', sid)
+                setLoading({
+                  refresh: true,
+                  message: t('status.loading', 'Loading Game list, please wait')
+                })
+                await refreshLibrary({
+                  fullRefresh: true,
+                  runInBackground: false
+                })
+                setLoading({ ...loading, refresh: false })
+              } catch (error) {
+                console.error(error)
+              }
+            }
+          })
+        }
       }
 
       const newUrl = () => {
@@ -61,25 +113,17 @@ export default function WebView({ isLogin }: Props) {
         console.log(webview.getURL())
       }
 
-      webview.addEventListener('dom-ready', async () => {
-        //        console.log(webview.getURL())
-        webview.selectAll()
-        webview.copy()
-        const text = clipboard.readText()
-        console.log({ text })
-      })
-
       webview.addEventListener('did-start-loading', loadstart)
       webview.addEventListener('did-stop-loading', loadstop)
       webview.addEventListener('did-navigate-in-page', newUrl)
       webview.addEventListener('did-redirect-navigation', redirectUrl)
     }
-  })
+  }, [])
 
   return (
     <div className="webViewContainer">
       <WebviewControls webview={webview} initURL={urls[startUrl]} />
-      {loading && <UpdateComponent />}
+      {loading.refresh && <UpdateComponent message={loading.message} />}
       <webview
         partition="persist:epicstore"
         src={urls[startUrl]}
