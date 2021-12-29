9f4325c0e4e595e0785d913f4f898b9cc80aa184
Flavio F Lima <flavioislima@gmail.com>
2021-12-29 21:28:49 +0100

    feat: add offline tag if epic is offline

--- electron/legendary/games.ts
+++ electron/legendary/games.ts
@@ -498,7 +498,8 @@ Categories=Game;
   }
 
   public async launch(launchArguments?: string) {
-    const isOffline = !(await isOnline())
+    const epicOffline = await isEpicOffline()
+    const isOffline = !(await isOnline()) || epicOffline
     let envVars = ''
     let gameMode: string
     const gameSettings = await this.getSettings()
