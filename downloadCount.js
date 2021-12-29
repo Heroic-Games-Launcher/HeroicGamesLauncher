68bcfa55e80812448b8178a5c6d82183e4493f45
Philip Wilk <wiryfuture@gmail.com>
2021-12-29 20:06:43 +0000

    rule does not exist

--- downloadCount.js
+++ downloadCount.js
@@ -1,4 +1,3 @@
-/* eslint-disable sort-keys-fix/sort-keys-fix */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const axios = require('axios')
 const { writeFileSync } = require('graceful-fs')
