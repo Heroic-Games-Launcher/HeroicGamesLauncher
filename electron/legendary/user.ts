a697a006d20fc391419ae4b6baed009ca89abe13
Flávio F Lima <flavioislima@gmail.com>
2022-01-16 15:29:38 +0100

    Update electron/legendary/user.ts
    Co-authored-by: Niklas <61798668+Nocccer@users.noreply.github.com>

--- electron/legendary/user.ts
+++ electron/legendary/user.ts
@@ -43,7 +43,7 @@ export class LegendaryUser {
         }
       })
       child.on('close', () => {
-        logInfo('finished login')
+        logInfo('finished login', LogPrefix.Legendary)
         res('finished')
       })
     })
