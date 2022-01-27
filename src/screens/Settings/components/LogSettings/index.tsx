1753179b9b0c89a94574678aa157cd2e1aab19b3
Nocccer <Nocccer@gmail.com>
2022-01-27 21:53:40 +0100

    Fixed handling if log file does not exist

--- src/screens/Settings/components/LogSettings/index.tsx
+++ src/screens/Settings/components/LogSettings/index.tsx
@@ -11,13 +11,19 @@ interface Props {
 
 export default function LogSettings({ isDefault, appName }: Props) {
   const { t } = useTranslation()
-  const [logFileContent, setLogFileContent] = useState<string[]>(['None'])
+  const [logFileContent, setLogFileContent] = useState<string[]>([])
+  const [logFileExist, setLogFileExist] = useState<boolean>(false)
 
   ipcRenderer
     .invoke('getCurrentLogContent', { isDefault, appName })
     .then((content: string[]) => {
       setLogFileContent(content)
-    })
+      setLogFileExist(true)
+    }).catch(() => {
+      setLogFileContent([t('setting.no-log-file', 'No log file found.')])
+      setLogFileExist(false)
+    }
+    )
 
   function showLogFileInFolder() {
     ipcRenderer.send('showLogFileInFolder', { isDefault, appName })
@@ -50,12 +56,12 @@ export default function LogSettings({ isDefault, appName }: Props) {
         })}
         <br />
       </span>
-      <button
+      {logFileExist && <button
         className={`setting button is-primary`}
         onClick={showLogFileInFolder}
       >
         {t('setting.show-log-folder', 'Show log file in folder')}
-      </button>
+      </button>}
     </>
   )
 }
