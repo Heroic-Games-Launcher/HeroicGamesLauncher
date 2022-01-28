3028fd2a59d14852c5b709dc787a046bb30671cd
Nocccer <Nocccer@gmail.com>
2022-01-28 11:19:24 +0100

    Fixed check if epic service is online
    - fixed also logs where a error was passend,
     without a format wrapper around. Else leads
     to tracebacks

--- electron/main.ts
+++ electron/main.ts
@@ -42,7 +42,7 @@ import {
   clearCache,
   errorHandler,
   execAsync,
-  isEpicOffline,
+  isEpicServiceOffline,
   getLegendaryVersion,
   getSystemInfo,
   handleExit,
@@ -528,7 +528,7 @@ ipcMain.handle('checkGameUpdates', () =>
   LegendaryLibrary.get().listUpdateableGames()
 )
 
-ipcMain.handle('getEpicGamesStatus', () => isEpicOffline())
+ipcMain.handle('getEpicGamesStatus', () => isEpicServiceOffline())
 
 // Not ready to be used safely yet.
 ipcMain.handle('updateAll', () => LegendaryLibrary.get().updateAllGames())
@@ -578,7 +578,7 @@ ipcMain.handle('getGameInfo', async (event, game) => {
     info.extra = await Game.get(game).getExtraInfo(info.namespace)
     return info
   } catch (error) {
-    logError(error, LogPrefix.Backend)
+    logError(`${error}`, LogPrefix.Backend)
   }
 })
 
@@ -591,7 +591,7 @@ ipcMain.handle('getInstallInfo', async (event, game) => {
     const info = await Game.get(game).getInstallInfo()
     return info
   } catch (error) {
-    logError(error, LogPrefix.Backend)
+    logError(`${error}`, LogPrefix.Backend)
     return {}
   }
 })
@@ -857,7 +857,7 @@ ipcMain.handle('install', async (event, params) => {
     return
   }
 
-  const epicOffline = await isEpicOffline()
+  const epicOffline = await isEpicServiceOffline()
   if (epicOffline) {
     dialog.showErrorBox(
       i18next.t('box.warning.title', 'Warning'),
@@ -915,7 +915,7 @@ ipcMain.handle('uninstall', async (event, game) => {
       notify({ title, body: i18next.t('notify.uninstalled') })
       logInfo('finished uninstalling', LogPrefix.Backend)
     })
-    .catch((error) => logError(error, LogPrefix.Backend))
+    .catch((error) => logError(`${error}`, LogPrefix.Backend))
 })
 
 ipcMain.handle('repair', async (event, game) => {
@@ -939,7 +939,7 @@ ipcMain.handle('repair', async (event, game) => {
         title,
         body: i18next.t('notify.error.reparing', 'Error Repairing')
       })
-      logError(error, LogPrefix.Backend)
+      logError(`${error}`, LogPrefix.Backend)
     })
 })
 
@@ -954,12 +954,12 @@ ipcMain.handle('moveInstall', async (event, [appName, path]: string[]) => {
       title,
       body: i18next.t('notify.error.move', 'Error Moving the Game')
     })
-    logError(error, LogPrefix.Backend)
+    logError(`${error}`, LogPrefix.Backend)
   }
 })
 
 ipcMain.handle('importGame', async (event, args) => {
-  const epicOffline = await isEpicOffline()
+  const epicOffline = await isEpicServiceOffline()
   if (epicOffline) {
     dialog.showErrorBox(
       i18next.t('box.warning.title', 'Warning'),
@@ -1008,7 +1008,7 @@ ipcMain.handle('updateGame', async (e, game) => {
     return
   }
 
-  const epicOffline = await isEpicOffline()
+  const epicOffline = await isEpicServiceOffline()
   if (epicOffline) {
     dialog.showErrorBox(
       i18next.t('box.warning.title', 'Warning'),
@@ -1153,7 +1153,7 @@ ipcMain.on('removeShortcut', async (event, appName: string) => {
 
 ipcMain.handle('syncSaves', async (event, args) => {
   const [arg = '', path, appName] = args
-  const epicOffline = await isEpicOffline()
+  const epicOffline = await isEpicServiceOffline()
   if (epicOffline) {
     logWarning('Epic is Offline right now, cannot sync saves!')
     return 'Epic is Offline right now, cannot sync saves!'
