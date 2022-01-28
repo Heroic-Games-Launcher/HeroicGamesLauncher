9ed37f51ce56875433b75cc8a8c20e0711b42694
Nocccer <Nocccer@gmail.com>
2022-01-28 16:28:03 +0100

    lint fix

--- src/screens/Settings/components/LogSettings/index.tsx
+++ src/screens/Settings/components/LogSettings/index.tsx
@@ -2,7 +2,7 @@ import './index.css'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import FolderOpenIcon from '@mui/icons-material/FolderOpen'
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 
 const { ipcRenderer } = window.require('electron')
 
@@ -43,24 +43,30 @@ export default function LogSettings({ isDefault, appName }: Props) {
 
   return (
     <>
-      {isDefault && <span className="setting log-buttongroup">
-        <button
-          className={`button log-buttons ${
-            defaultLast ? 'is-primary' : 'is-secondary'
-          }`}
-          onClick={() => {setDefaultLast(false)}}
-        >
-          {t('setting.log.current-log', 'Current Log')}
-        </button>
-        <button
-          className={`button log-buttons ${
-            !defaultLast ? 'is-primary' : 'is-secondary'
-          }`}
-          onClick={() => {setDefaultLast(true)}}
-        >
-          {t('setting.log.last-log', 'Last Log')}
-        </button>
-      </span>}
+      {isDefault && (
+        <span className="setting log-buttongroup">
+          <button
+            className={`button log-buttons ${
+              defaultLast ? 'is-primary' : 'is-secondary'
+            }`}
+            onClick={() => {
+              setDefaultLast(false)
+            }}
+          >
+            {t('setting.log.current-log', 'Current Log')}
+          </button>
+          <button
+            className={`button log-buttons ${
+              !defaultLast ? 'is-primary' : 'is-secondary'
+            }`}
+            onClick={() => {
+              setDefaultLast(true)
+            }}
+          >
+            {t('setting.log.last-log', 'Last Log')}
+          </button>
+        </span>
+      )}
       <span className="setting log-box">
         <br />
         {logFileContent?.split('\n').map((line, key) => {
@@ -93,16 +99,19 @@ export default function LogSettings({ isDefault, appName }: Props) {
             onClick={showLogFileInFolder}
             title={t('setting.log.show-in-folder', 'Show log file in folder.')}
           >
-            <FolderOpenIcon/>
+            <FolderOpenIcon />
           </button>
-          <button 
-          className={'button is-primary log-buttons'}
-          onClick={() => {
-            navigator.clipboard.writeText(logFileContent)
-          }}
-          title={t('setting.log.copy-to-clipboard', 'Copy log content to clipboard.')}
+          <button
+            className={'button is-primary log-buttons'}
+            onClick={() => {
+              navigator.clipboard.writeText(logFileContent)
+            }}
+            title={t(
+              'setting.log.copy-to-clipboard',
+              'Copy log content to clipboard.'
+            )}
           >
-            <ContentCopyIcon/>
+            <ContentCopyIcon />
           </button>
         </span>
       )}
