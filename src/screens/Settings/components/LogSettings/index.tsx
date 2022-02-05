7076f06a0bd51729875bd970d436e2e3058f8058
Nocccer <Nocccer@gmail.com>
2022-02-05 19:32:15 +0100

    Imrpoved log settings

--- src/screens/Settings/components/LogSettings/index.tsx
+++ src/screens/Settings/components/LogSettings/index.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import FolderOpenIcon from '@mui/icons-material/FolderOpen'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons'
 
 const { ipcRenderer } = window.require('electron')
 
@@ -16,25 +18,34 @@ export default function LogSettings({ isDefault, appName }: Props) {
   const [logFileContent, setLogFileContent] = useState<string>('')
   const [logFileExist, setLogFileExist] = useState<boolean>(false)
   const [defaultLast, setDefaultLast] = useState<boolean>(false)
+  const [refreshing, setRefreshing] = useState<boolean>(true)
 
-  const getLogContent = (defaultLast: boolean) => {
+  const getLogContent = () => {
     ipcRenderer
       .invoke('getLogContent', { isDefault, appName, defaultLast })
       .then((content: string) => {
         setLogFileContent(content)
         setLogFileExist(true)
+        setRefreshing(false)
       })
       .catch(() => {
         setLogFileContent(t('setting.log.no-file', 'No log file found.'))
         setLogFileExist(false)
+        setRefreshing(false)
       })
   }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      getLogContent(defaultLast)
-    }, 500)
-    return () => clearInterval(interval)
+    if (defaultLast || !isDefault) {
+      getLogContent()
+      return
+    } else {
+      getLogContent()
+      const interval = setInterval(() => {
+        getLogContent()
+      }, 1000)
+      return () => clearInterval(interval)
+    }
   }, [defaultLast])
 
   function showLogFileInFolder() {
@@ -47,43 +58,31 @@ export default function LogSettings({ isDefault, appName }: Props) {
         <span className="setting log-buttongroup toggleWrapper">
           <a
             className={`log-buttons ${!defaultLast ? 'log-choosen' : ''}`}
-            onClick={() => setDefaultLast(false)}
+            onClick={() => {
+              setRefreshing(true)
+              setDefaultLast(false)
+            }}
             title={t('setting.log.current-log')}
           >
             {t('setting.log.current-log', 'Current log')}
           </a>
           <a
             className={`log-buttons ${defaultLast ? 'log-choosen' : ''}`}
-            onClick={() => setDefaultLast(true)}
+            onClick={() => {
+              setRefreshing(true)
+              setDefaultLast(true)
+            }}
             title={t('setting.log.last-log')}
           >
             {t('setting.log.last-log', 'Last Log')}
           </a>
         </span>
       )}
-      <span className="setting log-box">
-        {logFileContent?.split('\n').map((line, key) => {
-          if (line.toLowerCase().includes('err')) {
-            return (
-              <p key={key} className="log-error">
-                {line}
-              </p>
-            )
-          } else if (line.toLowerCase().includes('warn')) {
-            return (
-              <p key={key} className="log-warning">
-                {line}
-              </p>
-            )
-          } else {
-            return (
-              <p key={key} className="log-info">
-                {line}
-              </p>
-            )
-          }
-        })}
-      </span>
+      {refreshing ? 
+        <span className="setting">
+          <FontAwesomeIcon className="icon" icon={faSyncAlt} />
+        </span>
+      : formatLogBox()}
       {logFileExist && (
         <span className="footerFlex">
           <a
@@ -126,4 +125,50 @@ export default function LogSettings({ isDefault, appName }: Props) {
       )}
     </>
   )
+
+  function formatLogBox() {
+    const maxLines = 1000
+    let sliced = false
+    let lines = logFileContent.split('\n')
+    if (lines.length > maxLines) {
+      lines = ['...', ...lines.slice(-maxLines)]
+      sliced = true
+    }
+
+    return (
+      <>
+        {sliced && (
+          <span className="setting long-log-hint">
+            {t(
+              'settings.log.long-log-hint',
+              'To long log! Showing only last 1000 lines.'
+            )}
+          </span>
+        )}
+        <span className="setting log-box">
+          {lines.map((line, key) => {
+            if (line.toLowerCase().includes('err')) {
+              return (
+                <p key={key} className="log-error">
+                  {line}
+                </p>
+              )
+            } else if (line.toLowerCase().includes('warn')) {
+              return (
+                <p key={key} className="log-warning">
+                  {line}
+                </p>
+              )
+            } else {
+              return (
+                <p key={key} className="log-info">
+                  {line}
+                </p>
+              )
+            }
+          })}
+        </span>
+      </>
+    )
+  }
 }
