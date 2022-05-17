5dacf3a9fd7a3538a4dd0b47384c4514bdbb4678
Ariel Juodziukynas <arieljuod@gmail.com>
2022-05-17 20:41:46 -0300

    Handle toggled state with css selector instead of class

--- src/components/UI/ToggleSwitch/index.tsx
+++ src/components/UI/ToggleSwitch/index.tsx
@@ -29,7 +29,6 @@ export default function ToggleSwitch(props: Props) {
       />
       <label
         className={classNames(`toggleSwitchWrapper Field ${extraClass}`, {
-          checked: value,
           isRTL
         })}
         htmlFor={htmlId}
