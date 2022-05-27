636b3ec23f6f2dcaf167b4c90f379173272d3444
Flavio F Lima <flavioislima@gmail.com>
2022-05-27 16:13:24 +0200

    fix: winetricks on flatpak

--- electron/tools.ts
+++ electron/tools.ts
@@ -210,7 +210,11 @@ export const Winetricks = {
 
     const { winePrefix, wineBin } = getWineFromProton(wine, isProton, prefix)
 
-    const command = `WINEPREFIX='${winePrefix}' WINE='${wineBin}' ${winetricks} -q`
+    // use wine instead of wine64 since it breaks on flatpak
+    const command = `WINEPREFIX='${winePrefix}' WINE='${wineBin.replace(
+      '64',
+      ''
+    )}' ${winetricks} -q`
 
     logInfo(['trying to run', command], LogPrefix.Backend)
     try {
