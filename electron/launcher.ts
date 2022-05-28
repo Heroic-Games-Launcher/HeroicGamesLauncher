ed58115acc63ce3df44aa7880b2d77b5fa07c5cc
Mathis Dröge <mathis.droege@ewe.net>
2022-05-28 15:37:02 +0200

    Don't use 'shell' on Mac anymore

--- electron/launcher.ts
+++ electron/launcher.ts
@@ -5,7 +5,7 @@ import i18next from 'i18next'
 import { existsSync, mkdirSync } from 'graceful-fs'
 import { join } from 'path'
 
-import { flatPakHome, isLinux, isMac } from './constants'
+import { flatPakHome, isLinux } from './constants'
 import {
   constructAndUpdateRPC,
   execAsync,
@@ -478,9 +478,7 @@ async function runLegendaryOrGogdlCommand(
   return new Promise((res, rej) => {
     const child = spawn(bin, commandParts, {
       cwd: runner.dir,
-      env: { ...options?.env, ...process.env },
-      // On Mac, launching some executables doesn't work without shell for some reason
-      shell: isMac
+      env: { ...options?.env, ...process.env }
     })
 
     const stdout: string[] = []
