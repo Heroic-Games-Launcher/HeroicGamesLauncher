0f5e62cb1ee2541916914e76ce42d869a79160d8
Delta2Force <deltacommandoforcegames@gmail.com>
2022-05-30 11:04:04 +0200

    Run lint on WineSettings

--- src/screens/Settings/components/WineSettings/index.tsx
+++ src/screens/Settings/components/WineSettings/index.tsx
@@ -263,9 +263,7 @@ export default function WineSettings({
         value={wineVersion.name}
         afterSelect={
           <>
-            <InfoBox text="infobox.wine-path">
-              {wineVersion.bin}
-            </InfoBox>
+            <InfoBox text="infobox.wine-path">{wineVersion.bin}</InfoBox>
             <InfoBox text="infobox.help">
               <span>{t('help.wine.part1')}</span>
               <ul>
