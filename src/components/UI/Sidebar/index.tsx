5ddc3ca4546be44aa48d035ac4270514ec2b09d6
Flavio F Lima <flavioislima@gmail.com>
2022-06-08 15:39:34 +0200

    feat: show heroic version on sidebar

--- src/components/UI/Sidebar/index.tsx
+++ src/components/UI/Sidebar/index.tsx
@@ -1,14 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { ipcRenderer } from 'src/helpers'
 import ContextProvider from 'src/state/ContextProvider'
 import CurrentDownload from './components/CurrentDownload'
 import SidebarLinks from './components/SidebarLinks'
 import './index.css'
 
 export default function Sidebar() {
+  const [heroicVersion, setHeroicVersion] = useState('')
+  const { t } = useTranslation()
   const { libraryStatus } = useContext(ContextProvider)
   const downloading = libraryStatus.filter(
     (g) => g.status === 'installing' || g.status === 'updating'
   )
+
+  useEffect(() => {
+    ipcRenderer
+      .invoke('getHeroicVersion')
+      .then((version) => setHeroicVersion(version))
+  }, [])
+
   return (
     <aside className="Sidebar">
       <SidebarLinks />
@@ -20,6 +31,10 @@ export default function Sidebar() {
             runner={g.runner}
           />
         ))}
+      </div>
+      <div className="heroicVersion">
+        <span>{t('info.heroic.version', 'Heroic Version')}: </span>
+        <strong>{heroicVersion}</strong>
       </div>
     </aside>
   )
