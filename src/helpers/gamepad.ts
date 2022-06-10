ba2fa9dbf1316044e1078da92ef9ff41b76d4b2a
Ariel Juodziukynas <arieljuod@gmail.com>
2022-06-10 19:09:36 -0300

    Read disableController setting at boot (#1438)

--- src/helpers/gamepad.ts
+++ src/helpers/gamepad.ts
@@ -1,5 +1,5 @@
 import { IpcRenderer } from 'electron'
-import { GamepadActionStatus } from 'src/types'
+import { AppSettings, GamepadActionStatus } from 'src/types'
 import {
   checkGameCube,
   checkPS3,
@@ -25,6 +25,12 @@ const SCROLL_REPEAT_DELAY = 50
 let controllerIsDisabled = false
 
 export const initGamepad = () => {
+  ipcRenderer
+    .invoke('requestSettings', 'default')
+    .then(({ disableController }: AppSettings) => {
+      controllerIsDisabled = disableController || false
+    })
+
   // store the current controllers
   let controllers: number[] = []
 
@@ -272,9 +278,9 @@ export const initGamepad = () => {
         console.log(`button ${button} pressed ${buttons[button].value}`)
     }
     for (const axis in axes) {
-      if (axes[axis] < -0.1 && axes[axis] >= -1)
+      if (axes[axis] < -0.2 && axes[axis] >= -1)
         console.log(`axis ${axis} activated negative`)
-      if (axes[axis] > 0.1 && axes[axis] <= 1)
+      if (axes[axis] > 0.2 && axes[axis] <= 1)
         console.log(`axis ${axis} activated positive`)
     }
   }
