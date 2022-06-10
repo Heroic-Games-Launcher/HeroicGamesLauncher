2ab160ff0f0126b023d7e5e41f0016f375606dee
Flavio F Lima <flavioislima@gmail.com>
2022-06-10 17:16:28 +0200

    fix: hide store filter when using one store only

--- src/components/UI/Header/index.tsx
+++ src/components/UI/Header/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { SearchBar } from 'src/components/UI'
 import StoreFilter from 'src/components/UI/StoreFilter'
+import ContextProvider from 'src/state/ContextProvider'
 import PlatformFilter from '../PlatformFilter'
 
 import './index.css'
 
 export default function Header() {
+  const { epic, gog } = useContext(ContextProvider)
+  const showStoreFilter = epic.username && gog.username
+
   return (
     <>
       <div className="Header">
         <span className="Header__filters">
-          <StoreFilter />
+          {showStoreFilter && <StoreFilter />}
           <PlatformFilter />
         </span>
         <div className="Header__search">
