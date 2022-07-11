5204dd95fa8dd01a46322e8e078153adfd640576
Mathis Dröge <mathis.droege@ewe.net>
2022-07-11 21:36:36 +0200

    Update tests

--- electron/__tests__/runtimes/utils.test.ts
+++ electron/__tests__/runtimes/utils.test.ts
@@ -1,47 +1,53 @@
 import axios from 'axios'
 import { getAssetDataFromDownload } from '../../wine/runtimes/util'
 // @ts-ignore: Don't know why ts complains about it.
-import { test_data } from './test_data/github-api-lutris-test-data.json'
+import { test_data } from './test_data/github-api-heroic-test-data.json'
 
-describe('Runtimes-utils', () => {
-  test('getAssetDataFromDownload - success', async () => {
+const testUrl =
+  'https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.3.9/Heroic-2.3.9.AppImage'
+
+describe('wine/runtimes/utils.ts', () => {
+  it('getAssetDataFromDownload - success', async () => {
     axios.get = jest.fn().mockResolvedValue(test_data)
 
-    await getAssetDataFromDownload(
-      'https://github.com/lutris/buildbot/releases/download/2021.11.20/eac_runtime.tar.xz'
-    ).then((response) => {
-      expect(response.name).toBe('eac_runtime.tar.xz')
-      expect(response.url).toBe(
-        'https://api.github.com/repos/lutris/buildbot/releases/assets/59731419'
-      )
-      expect(axios.get).toBeCalledWith(
-        'https://api.github.com/repos/lutris/buildbot/releases/tags/2021.11.20'
-      )
+    await expect(getAssetDataFromDownload(testUrl)).resolves.toMatchObject({
+      name: 'Heroic-2.3.9.AppImage',
+      url: 'https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases/assets/68579064'
     })
+    expect(axios.get).toBeCalledWith(
+      'https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases/tags/v2.3.9'
+    )
   })
 
   it('getAssetDataFromDownload - catch invalid URL/responses', async () => {
-    expect.assertions(3)
+    expect.assertions(4)
 
     // Empty URL
-    expect(getAssetDataFromDownload('')).rejects.toEqual(
+    await expect(getAssetDataFromDownload('')).rejects.toEqual(
+      Error('Invalid URL provided')
+    )
+
+    // Empty data
+    axios.get = jest.fn().mockResolvedValue({ data: {}, status: 200 })
+    await expect(getAssetDataFromDownload(testUrl)).rejects.toEqual(
       Error('Asset metadata could not be found')
     )
 
-    // No data
-    axios.get = jest.fn().mockResolvedValue({})
-    expect(
-      getAssetDataFromDownload(
-        'https://github.com/lutris/buildbot/releases/download/2021.11.20/eac_runtime.tar.xz'
-      )
-    ).rejects.toEqual(Error('Asset metadata could not be found'))
+    // HTTP error code
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: { assets: [] }, status: 404 })
+    await expect(getAssetDataFromDownload(testUrl)).rejects.toEqual(
+      Error('Got HTTP error code 404')
+    )
 
-    // Empty data
-    axios.get = jest.fn().mockResolvedValue({ data: {} })
-    expect(
-      getAssetDataFromDownload(
-        'https://github.com/lutris/buildbot/releases/download/2021.11.20/eac_runtime.tar.xz'
-      )
-    ).rejects.toEqual(Error('Asset metadata could not be found'))
+    // Axios error
+    // https://axios-http.com/docs/handling_errors
+    axios.get = jest.fn().mockRejectedValue({
+      toJSON: () => '{ "message": "Some error message" }'
+    })
+    await expect(getAssetDataFromDownload(testUrl)).rejects.toEqual(
+      Error('Failed to access GitHub API: { "message": "Some error message" }')
+    )
   })
 })
