31de0b6c9e01a68457e7573bdf2e1c00574f6eb3
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2022-07-17 11:08:50 -0400

    add to beta branch

--- electron/shortcuts/shortcuts/shortcuts.ts
+++ electron/shortcuts/shortcuts/shortcuts.ts
@@ -1,5 +1,5 @@
 import { app, shell } from 'electron'
-import { unlink, writeFile } from 'graceful-fs'
+import { unlink, writeFile, chmod } from 'graceful-fs'
 import { logError, logInfo, LogPrefix } from '../../logger/logger'
 import { GlobalConfig } from '../../config'
 import { removeSpecialcharacters } from '../../utils'
@@ -41,6 +41,9 @@ Categories=Game;
       if (addDesktopShortcuts || fromMenu) {
         writeFile(desktopFile, shortcut, () => {
           logInfo(`Shortcut saved on ${desktopFile}`, LogPrefix.Backend)
+        })
+        chmod(desktopFile, '+x', () => {
+          logInfo(`Shortcut permissions set on ${desktopFile}`, LogPrefix.Backend)
         })
       }
       if (addStartMenuShortcuts || fromMenu) {
