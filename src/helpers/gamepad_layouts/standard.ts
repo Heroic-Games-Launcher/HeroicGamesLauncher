c4f124cade2aedeb3b8b89e094f71063c2fce7cb
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2022-09-03 16:41:19 -0400

    change variable names of standard layout

--- src/helpers/gamepad_layouts/standard.ts
+++ src/helpers/gamepad_layouts/standard.ts
@@ -6,10 +6,10 @@ export function checkStandard(
   controllerIndex: number,
   checkAction: (action: string, pressed: boolean, ctrlIdx: number) => void
 ) {
-  const A = buttons[0],
-    B = buttons[1],
-    X = buttons[2],
-    Y = buttons[3],
+  const mainButton = buttons[0], //Xbox: A, PS: Cross
+    backButton = buttons[1], //Xbox: B, PS: Circle
+    subMenuButton = buttons[2], // Xbox: X, PS: Square
+    altButton = buttons[3], // Xbox: Y, PS: Triangle
     // LB = buttons[4],
     // RB = buttons[5],
     // LT = buttons[6], // has .value
@@ -30,9 +30,9 @@ export function checkStandard(
 
   // use the `?` operator here since this layout is used as fallback
   // and mapping can be incorrect
-  checkAction('mainAction', A?.pressed, controllerIndex)
-  checkAction('back', B?.pressed, controllerIndex)
-  checkAction('altAction', Y?.pressed, controllerIndex)
+  checkAction('mainAction', mainButton?.pressed, controllerIndex)
+  checkAction('back', backButton?.pressed, controllerIndex)
+  checkAction('altAction', altButton?.pressed, controllerIndex)
   checkAction('leftStickLeft', leftAxisX < -0.5, controllerIndex)
   checkAction('leftStickRight', leftAxisX > 0.5, controllerIndex)
   checkAction('leftStickUp', leftAxisY < -0.5, controllerIndex)
@@ -45,5 +45,5 @@ export function checkStandard(
   checkAction('padDown', down?.pressed, controllerIndex)
   checkAction('padLeft', left?.pressed, controllerIndex)
   checkAction('padRight', right?.pressed, controllerIndex)
-  checkAction('rightClick', X?.pressed, controllerIndex)
+  checkAction('rightClick', subMenuButton?.pressed, controllerIndex)
 }
