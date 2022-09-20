5b9299e0bcd8840810894ccf8adef3bf741e3ee0
Flavio F Lima <flavioislima@gmail.com>
2022-09-20 23:01:48 +0200

    chore: split methods from object

--- src/backend/sideload/games.ts
+++ src/backend/sideload/games.ts
@@ -18,96 +18,95 @@ interface SideloadGame {
   }
 }
 
-const SideloadGames = {
-  getGameInfo(appName: string): GameInfo {
-    return libraryStore.get(appName, {}) as GameInfo
-  },
+export function getGameInfo(appName: string): GameInfo {
+  return libraryStore.get(appName, {}) as GameInfo
+}
 
-  async getSettings(appName: string): Promise<GameSettings> {
-    return (
-      GameConfig.get(appName).config ||
-      (await GameConfig.get(appName).getSettings())
-    )
-  },
+export async function getSettings(appName: string): Promise<GameSettings> {
+  return (
+    GameConfig.get(appName).config ||
+    (await GameConfig.get(appName).getSettings())
+  )
+}
 
-  install(app: SideloadGame): void {
-    return libraryStore.set(app.app_name, app)
-  },
-  async addShortcuts(): Promise<void> {
-    throw new Error('Method not implemented.')
-  },
+export function install(app: SideloadGame): void {
+  return libraryStore.set(app.app_name, app)
+}
 
-  async launch(
-    appName: string,
-    launchArguments?: string | undefined
-  ): Promise<boolean> {
-    const {
-      install: { executable }
-    } = this.getGameInfo(appName)
-    if (executable) {
-      console.log({ launchArguments, executable })
-    }
-    return false
-  },
+export async function addShortcuts(): Promise<void> {
+  throw new Error('Method not implemented.')
+}
 
-  async moveInstall(appName: string, newInstallPath: string): Promise<string> {
-    const {
-      install: { install_path },
-      title
-    } = this.getGameInfo(appName)
+export async function launch(
+  appName: string,
+  launchArguments?: string | undefined
+): Promise<boolean> {
+  const {
+    install: { executable }
+  } = getGameInfo(appName)
+  if (executable) {
+    console.log({ launchArguments, executable })
+  }
+  return false
+}
 
-    if (!install_path) {
-      return ''
-    }
+export async function moveInstall(
+  appName: string,
+  newInstallPath: string
+): Promise<string> {
+  const {
+    install: { install_path },
+    title
+  } = getGameInfo(appName)
 
-    if (isWindows) {
-      newInstallPath += '\\' + install_path.split('\\').at(-1)
-    } else {
-      newInstallPath += '/' + install_path.split('/').at(-1)
-    }
+  if (!install_path) {
+    return ''
+  }
 
-    logInfo(`Moving ${title} to ${newInstallPath}`, {
-      prefix: LogPrefix.Backend
+  if (isWindows) {
+    newInstallPath += '\\' + install_path.split('\\').at(-1)
+  } else {
+    newInstallPath += '/' + install_path.split('/').at(-1)
+  }
+
+  logInfo(`Moving ${title} to ${newInstallPath}`, {
+    prefix: LogPrefix.Backend
+  })
+  await execAsync(`mv -f '${install_path}' '${newInstallPath}'`, execOptions)
+    .then(() => {
+      SideLoadLibrary.get().changeGameInstallPath(appName, newInstallPath)
+      logInfo(`Finished Moving ${title}`, { prefix: LogPrefix.Backend })
     })
-    await execAsync(`mv -f '${install_path}' '${newInstallPath}'`, execOptions)
-      .then(() => {
-        SideLoadLibrary.get().changeGameInstallPath(appName, newInstallPath)
-        logInfo(`Finished Moving ${title}`, { prefix: LogPrefix.Backend })
-      })
-      .catch((error) => logError(`${error}`, { prefix: LogPrefix.Backend }))
-    return newInstallPath
-  },
+    .catch((error) => logError(`${error}`, { prefix: LogPrefix.Backend }))
+  return newInstallPath
+}
+export async function stop(appName: string): Promise<void> {
+  const {
+    install: { executable }
+  } = getGameInfo(appName)
 
-  async stop(appName: string): Promise<void> {
-    const {
-      install: { executable }
-    } = this.getGameInfo(appName)
-
-    if (executable) {
-      killPattern(executable)
-    }
-  },
-
-  uninstall(appName: string): void {
-    return libraryStore.delete(appName)
-  },
-
-  isNative(appName: string): boolean {
-    const gameInfo = this.getGameInfo(appName)
-    if (isWindows) {
-      return true
-    }
-
-    if (isMac && gameInfo.install.platform === 'osx') {
-      return true
-    }
-
-    if (isLinux && gameInfo.install.platform === 'linux') {
-      return true
-    }
-
-    return false
+  if (executable) {
+    killPattern(executable)
   }
 }
 
-export default SideloadGames
+export function uninstall(appName: string): void {
+  return libraryStore.delete(appName)
+}
+
+export function isNative(appName: string): boolean {
+  const gameInfo = getGameInfo(appName)
+  if (isWindows) {
+    return true
+  }
+
+  if (isMac && gameInfo.install.platform === 'osx') {
+    return true
+  }
+
+  if (isLinux && gameInfo.install.platform === 'linux') {
+    return true
+  }
+
+  return false
+}
