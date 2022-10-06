324c08b01570ad01342dfac98636b777b4f6f04f
Ariel Juodziukynas <arieljuod@gmail.com>
2022-10-06 20:45:26 -0300

    remove React import

--- src/frontend/hooks/useSetting.ts
+++ src/frontend/hooks/useSetting.ts
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import SettingsContext from 'frontend/screens/Settings/SettingsContext'
 
 const useSetting = <T>(key: string, fallback: T): [T, (newVal: T) => void] => {
