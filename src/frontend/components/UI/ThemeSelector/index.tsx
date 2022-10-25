4abf4cf5d3b8b7fbff50ada5376c0ab386009dfa
Redromnon <rishimoharir@gmail.com>
2022-10-25 13:15:10 +0530

    Add Nord Light theme

--- src/frontend/components/UI/ThemeSelector/index.tsx
+++ src/frontend/components/UI/ThemeSelector/index.tsx
@@ -22,6 +22,8 @@ export const ThemeSelector = () => {
       <option value="marine-classic">Marine Classic</option>
       <option value="zombie">Zombie</option>
       <option value="zombie-classic">Zombie Classic</option>
+      <option value="nord-light">Nord Light</option>
+      <option value="nord-dark">Noard Dark</option>
     </SelectField>
   )
 }
