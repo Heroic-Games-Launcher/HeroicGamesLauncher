c9c7ad724c077dcb9fb576c9bdded104fb9e411e
Flavio F Lima <flavioislima@gmail.com>
2022-11-19 22:25:14 +0100

    chore: log

--- src/frontend/components/UI/HowLongToBeat/index.tsx
+++ src/frontend/components/UI/HowLongToBeat/index.tsx
@@ -26,8 +26,6 @@ export default function HowLongToBeat({ title }: Props) {
     return null
   }
 
-  console.log(howLongToBeatInfo)
-
   const { gameplayMain, gameplayMainExtra, gameplayCompletionist } =
     howLongToBeatInfo || {}
 
