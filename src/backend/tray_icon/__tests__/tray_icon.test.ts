6dfd899cf8e80ffe0e0dc103d10919f8d7a894ef
Ariel Juodziukynas <arieljuod@gmail.com>
2022-12-16 23:18:58 -0300

    Limit recent games on update, refactor

--- src/backend/tray_icon/__tests__/tray_icon.test.ts
+++ src/backend/tray_icon/__tests__/tray_icon.test.ts
@@ -1,15 +1,24 @@
 import { BrowserWindow } from 'electron'
 import { contextMenu, getIcon, initTrayIcon } from '../tray_icon'
 import { backendEvents } from '../../backend_events'
-import { setRecentGames } from '../../recent_games/recent_games'
 import { GlobalConfig } from '../../config'
+import { RecentGame } from '../../../common/types'
+import { configStore } from '../../constants'
+import { wait } from '../../utils'
 
 jest.mock('../../logger/logfile')
 jest.mock('../../config')
-jest.mock('../../recent_games/recent_games')
 
 describe('TrayIcon', () => {
   const mainWindow = new BrowserWindow()
+
+  const setRecentGames = (games: RecentGame[]) => {
+    configStore.get = () => games
+  }
+
+  afterEach(() => {
+    configStore.get = jest.fn()
+  })
 
   describe('content', () => {
     describe('contextMenu', () => {
@@ -41,38 +50,139 @@ describe('TrayIcon', () => {
           click: expect.any(Function)
         })
       })
+      describe('when recent games change', () => {
+        it('updates the content when recent games change', async () => {
+          setRecentGames([{ title: 'game 1', appName: '12345' }])
+
+          const appIcon = await initTrayIcon(mainWindow)
+
+          expect(appIcon['menu'][0]).toEqual({
+            click: expect.any(Function),
+            label: 'game 1'
+          })
+
+          expect(appIcon['menu'][1]).toEqual({
+            type: 'separator'
+          })
+
+          backendEvents.emit('recentGamesChanged', [
+            { title: 'game 2', appName: '67890' },
+            { title: 'game 1', appName: '12345' }
+          ])
+
+          // wait for a moment since the event handler is async
+          await wait(5)
+
+          expect(appIcon['menu'][0]).toEqual({
+            click: expect.any(Function),
+            label: 'game 2'
+          })
+
+          expect(appIcon['menu'][1]).toEqual({
+            click: expect.any(Function),
+            label: 'game 1'
+          })
+
+          expect(appIcon['menu'][2]).toEqual({
+            type: 'separator'
+          })
+        })
+
+        it('limits the games based on config', async () => {
+          // limits to maxRecentGames config
+          GlobalConfig['setConfigValue']('maxRecentGames', 3)
+
+          setRecentGames([])
+
+          const appIcon = await initTrayIcon(mainWindow)
+
+          expect(appIcon['menu'][0]).toEqual({
+            type: 'separator'
+          })
+
+          backendEvents.emit('recentGamesChanged', [
+            { title: 'game 1', appName: '1' },
+            { title: 'game 2', appName: '2' },
+            { title: 'game 3', appName: '3' },
+            { title: 'game 4', appName: '4' },
+            { title: 'game 5', appName: '5' }
+          ])
+
+          // wait for a moment since the event handler is async
+          await wait(5)
+
+          const items = appIcon['menu']
+
+          expect(items[0]).toEqual({
+            click: expect.any(Function),
+            label: 'game 1'
+          })
+
+          expect(items[1]).toEqual({
+            click: expect.any(Function),
+            label: 'game 2'
+          })
+
+          expect(items[2]).toEqual({
+            click: expect.any(Function),
+            label: 'game 3'
+          })
+
+          expect(items[3]).toEqual({
+            type: 'separator'
+          })
+        })
+      })
     })
 
-    it('updates the content when recent games change', async () => {
-      setRecentGames([{ title: 'game 1', appName: '12345' }])
+    it('limits the number of recent games to show at creation', async () => {
+      const recentGames = [
+        { title: 'game 1', appName: '1' },
+        { title: 'game 2', appName: '2' },
+        { title: 'game 3', appName: '3' },
+        { title: 'game 4', appName: '4' },
+        { title: 'game 5', appName: '5' },
+        { title: 'game 6', appName: '6' },
+        { title: 'game 7', appName: '7' },
+        { title: 'game 8', appName: '8' },
+        { title: 'game 9', appName: '9' }
+      ]
+
+      setRecentGames(recentGames)
+
+      // defaults to 5
+      GlobalConfig['setConfigValue']('maxRecentGames', undefined)
 
       const appIcon = await initTrayIcon(mainWindow)
 
-      expect(appIcon['menu'][0]).toEqual({
+      const items = appIcon['menu']
+
+      expect(items[0]).toEqual({
         click: expect.any(Function),
         label: 'game 1'
       })
 
-      expect(appIcon['menu'][1]).toEqual({
-        type: 'separator'
-      })
-
-      backendEvents.emit('recentGamesChanged', [
-        { title: 'game 2', appName: '67890' },
-        { title: 'game 1', appName: '12345' }
-      ])
-
-      expect(appIcon['menu'][0]).toEqual({
+      expect(items[1]).toEqual({
         click: expect.any(Function),
         label: 'game 2'
       })
 
-      expect(appIcon['menu'][1]).toEqual({
+      expect(items[2]).toEqual({
         click: expect.any(Function),
-        label: 'game 1'
+        label: 'game 3'
       })
 
-      expect(appIcon['menu'][2]).toEqual({
+      expect(items[3]).toEqual({
+        click: expect.any(Function),
+        label: 'game 4'
+      })
+
+      expect(items[4]).toEqual({
+        click: expect.any(Function),
+        label: 'game 5'
+      })
+
+      expect(items[5]).toEqual({
         type: 'separator'
       })
     })
