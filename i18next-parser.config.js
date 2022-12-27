6efdd05c969a5196e947180f4d87b28b35821b83
Nocccer <Nocccer@gmail.com>
2022-12-27 18:36:11 +0100

    prettier fix

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -53,9 +53,7 @@ module.exports = {
   lineEnding: 'auto',
   // Control the line ending. See options at https://github.com/ryanve/eol
 
-  locales: [
-    'en'
-  ],
+  locales: ['en'],
   // An array of the locales in your applications
 
   namespaceSeparator: ':',
