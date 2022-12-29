6be0042a81f801b884f26d8cfe475e19cfc7dca9
Flavio F Lima <flavioislima@gmail.com>
2022-12-29 15:30:51 +0100

    feat: add className prop to dialog content

--- src/frontend/components/UI/Dialog/components/DialogContent.tsx
+++ src/frontend/components/UI/Dialog/components/DialogContent.tsx
@@ -2,8 +2,12 @@ import React, { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
+  className?: string
 }
 
-export const DialogContent: React.FC<Props> = ({ children }: Props) => {
-  return <div className="Dialog__content">{children}</div>
+export const DialogContent: React.FC<Props> = ({
+  children,
+  className
+}: Props) => {
+  return <div className={`Dialog__content ${className}`}>{children}</div>
 }
