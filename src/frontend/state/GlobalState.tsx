74de805a6f302ff5d0fa0c58c5ff314f11e2225c
Flávio F Lima <flavioislima@gmail.com>
2023-01-04 12:25:35 +0100

    [UI/UX] Add filter for non-available games (#2281)
    * feat: add nonAvailable types
    
    * feat: added logic to store non available games
    
    * feat: add icon to show/hide list
    
    * i18n: updated keys
    
    * feat: store state on storage
    
    * fix: clear list of games on startup
    
    * chore: do not store button state on storage
    
    * other: move check available to gamelist
    
    * fix: use useMemo instead of useEffect
    
    * fix: useEffect instead of useMemo
    
    * chore: log

--- src/frontend/state/GlobalState.tsx
+++ src/frontend/state/GlobalState.tsx
@@ -77,6 +77,7 @@ interface StateProps {
   hiddenGames: HiddenGame[]
   showHidden: boolean
   showFavourites: boolean
+  showNonAvailable: boolean
   favouriteGames: FavouriteGame[]
   theme: string
   zoomPercent: number
@@ -143,6 +144,7 @@ export class GlobalState extends PureComponent<Props> {
       (configStore.get('games.hidden', []) as Array<HiddenGame>) || [],
     showHidden: JSON.parse(storage.getItem('show_hidden') || 'false'),
     showFavourites: JSON.parse(storage.getItem('show_favorites') || 'false'),
+    showNonAvailable: true,
     sidebarCollapsed: JSON.parse(
       storage.getItem('sidebar_collapsed') || 'false'
     ),
@@ -220,6 +222,10 @@ export class GlobalState extends PureComponent<Props> {
 
   setShowFavourites = (value: boolean) => {
     this.setState({ showFavourites: value })
+  }
+
+  setShowNonAvailable = (value: boolean) => {
+    this.setState({ showNonAvailable: value })
   }
 
   setSideBarCollapsed = (value: boolean) => {
@@ -746,6 +752,7 @@ export class GlobalState extends PureComponent<Props> {
           },
           setShowHidden: this.setShowHidden,
           setShowFavourites: this.setShowFavourites,
+          setShowNonAvailable: this.setShowNonAvailable,
           favouriteGames: {
             list: this.state.favouriteGames,
             add: this.addGameToFavourites,
