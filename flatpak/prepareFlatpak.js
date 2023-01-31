e7d97f58e71b978ff7700e365d4ef39d161b2b5d
Flávio F Lima <flavioislima@gmail.com>
2023-01-31 19:24:11 +0100

    [Tech] Flatpak Build Workflow (#2374)
    * ci: add flatpak build file
    
    * chore: path and bump gnome image
    
    * chore: move build file to flatpak folder
    
    * chore: try with local appImage
    
    * fix: yml lint
    
    * chore: use npm
    
    * chore: appimage path
    
    * chore: mv appimage to flatpak folder
    
    * chore: try without rsync
    
    * Add echo for debugging
    
    * New debug messages
    
    * Possible fix for flatpak workflow
    
    * Fix workflow
    
    * Fixes
    
    * Add flatpak tempaltes for auto updates
    
    * prettier fix
    
    * Fix paths
    
    * Fix gitignore
    
    * Try with secrets
    
    * Make script executable
    
    * Add priviledged
    
    * typo
    
    * Removed privilegde
    
    * Remove files before
    
    * With sudo
    
    * Remove sudo
    
    * Copy all flatpak files to build
    
    * Fix workflow
    
    * prettier fix
    
    * Fix vite does not like flatpak in build
    
    * fix: flatpak folder on workflow
    
    * chore: logging and yml path
    
    * chore: get back old build path
    
    * chore: package.json
    
    * chore: try again with old config
    
    * Clean up
    
    ---------
    
    Co-authored-by: Nocccer <Nocccer@gmail.com>

---
+++ flatpak/prepareFlatpak.js
@@ -0,0 +1,60 @@
+const { default: axios } = require('axios')
+const fs = require('fs')
+const package = require('../package.json')
+const child_process = require('child_process')
+const os = require('os')
+const crypto = require('crypto')
+
+if (require.main === module) {
+    main();
+}
+
+async function main() {
+    let placeholder = ""
+    let releaseTime = ""
+    if (process.argv[2] === "release") {
+        const { data } = await axios.get("https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases/latest")
+        const appimage = data.assets.find((asset) => asset.browser_download_url.includes(".AppImage"))
+        const outputFile = `${os.tmpdir()}/Heroic.AppImage`
+        child_process.spawnSync("curl", ["-L", appimage.browser_download_url, "-o", outputFile, "--create-dirs"])
+        const outputContent = fs.readFileSync(outputFile)
+        const hashSum = crypto.createHash('sha512');
+        hashSum.update(outputContent);
+        const sha512 = hashSum.digest('hex');
+        fs.rmSync(outputFile)
+
+        placeholder = [
+            "type: file",
+            `url: ${appimage.browser_download_url}`,
+            `sha512: ${sha512}`
+        ].join("\n        ")
+        releaseTime = data.published_at.split('T')[0]
+    } else {
+        placeholder = [
+            "type: file",
+            `path: "../dist/Heroic-${package.version}.AppImage"`
+        ].join("\n        ")
+        releaseTime = new Date().toISOString().split('T')[0]
+    }
+
+    // generate flatpak-build
+    if (!fs.existsSync("./flatpak-build")) {
+        fs.mkdirSync('./flatpak-build', { recursive: true })
+    }
+
+    // generate manifest
+    let templateManifest = fs.readFileSync(`./flatpak/templates/com.heroicgameslauncher.hgl.yml.template`, { encoding: 'utf-8' })
+    templateManifest = templateManifest.replace("${heroic-app-image}", placeholder)
+    fs.writeFileSync("./flatpak-build/com.heroicgameslauncher.hgl.yml", templateManifest)
+
+    // generate metainfo
+    let templateMetaInfo = fs.readFileSync(`./flatpak/templates/com.heroicgameslauncher.hgl.metainfo.xml.template`, { encoding: 'utf-8' })
+    templateMetaInfo = templateMetaInfo.replace("${heroic-version}", `v${package.version}`).replace("${heroic-release-date}", releaseTime)
+    fs.writeFileSync("./flatpak-build/com.heroicgameslauncher.hgl.metainfo.xml", templateMetaInfo)
+
+    // copy extra files
+    fs.copyFileSync("./flatpak/com.heroicgameslauncher.hgl.desktop", "./flatpak-build/com.heroicgameslauncher.hgl.desktop")
+    fs.copyFileSync("./flatpak/com.heroicgameslauncher.hgl.png", "./flatpak-build/com.heroicgameslauncher.hgl.png")
+    fs.copyFileSync("./flatpak/flathub.json", "./flatpak-build/flathub.json")
+    fs.cpSync("./flatpak/patches", "./flatpak-build/patches", { recursive: true })
+}
