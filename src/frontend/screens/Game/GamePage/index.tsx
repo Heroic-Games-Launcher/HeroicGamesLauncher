037ad96000e44deaa559311704dba3dd1502d257
Flavio F Lima <flavioislima@gmail.com>
2023-02-27 00:01:29 +0100

    fix: game requirements

--- src/frontend/screens/Game/GamePage/index.tsx
+++ src/frontend/screens/Game/GamePage/index.tsx
@@ -221,15 +221,16 @@ export default React.memo(function GamePage(): JSX.Element | null {
   }, [status, epic.library, gog.library, gameInfo, isSettingsModalOpen])
 
   useEffect(() => {
-    const id = runner === 'gog' ? appName : undefined
-    window.api.getWikiGameInfo(gameInfo.title, id).then((info: WikiInfo) => {
-      if (
-        info &&
-        (info.applegamingwiki || info.howlongtobeat || info.pcgamingwiki)
-      ) {
-        setWikiGameInfo(info)
-      }
-    })
+    window.api
+      .getWikiGameInfo(gameInfo.title, appName, runner)
+      .then((info: WikiInfo) => {
+        if (
+          info &&
+          (info.applegamingwiki || info.howlongtobeat || info.pcgamingwiki)
+        ) {
+          setWikiGameInfo(info)
+        }
+      })
   }, [appName])
 
   function handleUpdate() {
