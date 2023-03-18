66911cdad6a903d9b58e43f15573e02c41286d21
Mathis Dröge <mathis.droege@ewe.net>
2023-03-18 19:38:31 +0100

    Return an empty string if path calculation fails
    The original idea with this returning the path with variables was that
    the user could then figure out the actual path more easily. In reality,
    most people just assume that it's correct (can't fault them for that
    either really), and head to our Discord wondering why it doesn't work

--- src/backend/save_sync.ts
+++ src/backend/save_sync.ts
@@ -89,14 +89,16 @@ async function getDefaultLegendarySavePath(appName: string): Promise<string> {
 
   // If the save path was computed successfully, Legendary will have saved
   // this path in `installed.json` (so the GameInfo)
-  const { save_path: new_save_path, save_folder } =
-    LegendaryLibrary.get().getGameInfo(appName, true)!
+  const { save_path: new_save_path } = LegendaryLibrary.get().getGameInfo(
+    appName,
+    true
+  )!
   if (!new_save_path) {
     logError(
       ['Unable to compute default save path for', appName],
       LogPrefix.Legendary
     )
-    return save_folder
+    return ''
   }
   logInfo(['Computed save path:', new_save_path], LogPrefix.Legendary)
   return new_save_path
