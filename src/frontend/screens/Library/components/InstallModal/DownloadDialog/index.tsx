HyperPlay-Gaming/hyperplay-desktop-client

[Tech] Refactor game managers (#183)
* init

* refactor gog, legendary, hyperplay games.ts

* update submodule to latest main commits

* refactor game manager games module

* update types, refactor library manager

* fix diskWrite merge

* fix more types

* rm bin exec override

* move store managers into storeManagers folder

* fix data types for hp games, add listUpdateableGames

* refactor library refresh

* refactor runWineCommand and hasGame

* rm hasUpdate, fix hp store install version

* rm refreshInstalled from library interface

* rm unused updateAllGames

* fix stop, add unzipping status, add inject process name

* change unzipping to extracting, add process names, fix restricted char bugs

* fix legendary and gog download status

* fix gog install, uninstall

* fix default install path setting

* fix proton/wine

* fix gog and legendary import

* update gameInfo and use gameInfo to inject HP Games

* move storeManager maps and fxns out of main

* fix legendary moveInstall and changeInstallPath

* refresh gog when app is ready

* fix changeGameInstallPath

* fix uninstall when gog game is already moved

* fix remaining merge conflicts

* add one conditional method call

src/frontend/screens/Library/components/InstallModal/DownloadDialog/index.tsx
@@ -91,8 +91,12 @@ function getUniqueKey(sdl: SelectiveDownload) {
 }
 
 const userHome = configStore.get('userHome', '')
-const { defaultInstallPath = `${userHome}/Games/HyperPlay` } = {
-  ...configStore.get_nodefault('settings')
+
+function getDefaultInstallPath() {
+  const { defaultInstallPath = `${userHome}/Games/HyperPlay` } = {
+    ...configStore.get_nodefault('settings')
+  }
+  return defaultInstallPath
 }
 
 export default function DownloadDialog({
@@ -121,7 +125,7 @@ export default function DownloadDialog({
   const [installLanguages, setInstallLanguages] = useState(Array<string>())
   const [installLanguage, setInstallLanguage] = useState('')
   const [installPath, setInstallPath] = useState(
-    previousProgress.folder || defaultInstallPath
+    previousProgress.folder || getDefaultInstallPath()
   )
   const gameStatus: GameStatus = libraryStatus.filter(
     (game: GameStatus) => game.appName === appName
@@ -437,7 +441,7 @@ export default function DownloadDialog({
         <TextInputWithIconField
           htmlId="setinstallpath"
           label={t('install.path', 'Select Install Path')}
-          placeholder={defaultInstallPath}
+          placeholder={getDefaultInstallPath()}
           value={installPath.replaceAll("'", '')}
           onChange={(event) => setInstallPath(event.target.value)}
           icon={<FontAwesomeIcon icon={faFolderOpen} />}
@@ -447,9 +451,9 @@ export default function DownloadDialog({
                 buttonLabel: t('box.choose'),
                 properties: ['openDirectory'],
                 title: t('install.path'),
-                defaultPath: defaultInstallPath
+                defaultPath: getDefaultInstallPath()
               })
-              .then((path) => setInstallPath(path || defaultInstallPath))
+              .then((path) => setInstallPath(path || getDefaultInstallPath()))
           }
           afterInput={
             gameInstallInfo?.manifest?.download_size ? (

Heroic-Games-Launcher/HeroicGamesLauncher

src/frontend/screens/Library/components/InstallModal/DownloadDialog/index.tsx
@@ -94,6 +94,13 @@ function getUniqueKey(sdl: SelectiveDownload) {
 
 const userHome = configStore.get('userHome', '')
 
+function getDefaultInstallPath() {
+  const { defaultInstallPath = `${userHome}/Games/Heroic` } = {
+    ...configStore.get_nodefault('settings')
+  }
+  return defaultInstallPath
+}
+
 export default function DownloadDialog({
   backdropClick,
   appName,
@@ -120,12 +127,8 @@ export default function DownloadDialog({
   const [installLanguages, setInstallLanguages] = useState(Array<string>())
   const [installLanguage, setInstallLanguage] = useState('')
 
-  const { defaultInstallPath = '' } = {
-    ...configStore.get_nodefault('settings')
-  }
-
   const [installPath, setInstallPath] = useState(
-    previousProgress.folder || defaultInstallPath || `${userHome}/Games/Heroic`
+    previousProgress.folder || getDefaultInstallPath()
   )
   const gameStatus: GameStatus = libraryStatus.filter(
     (game: GameStatus) => game.appName === appName
@@ -441,7 +444,7 @@ export default function DownloadDialog({
         <TextInputWithIconField
           htmlId="setinstallpath"
           label={t('install.path', 'Select Install Path')}
-          placeholder={defaultInstallPath}
+          placeholder={getDefaultInstallPath()}
           value={installPath.replaceAll("'", '')}
           onChange={(event) => setInstallPath(event.target.value)}
           icon={<FontAwesomeIcon icon={faFolderOpen} />}
@@ -451,9 +454,9 @@ export default function DownloadDialog({
                 buttonLabel: t('box.choose'),
                 properties: ['openDirectory'],
                 title: t('install.path'),
-                defaultPath: defaultInstallPath
+                defaultPath: getDefaultInstallPath()
               })
-              .then((path) => setInstallPath(path || defaultInstallPath))
+              .then((path) => setInstallPath(path || getDefaultInstallPath()))
           }
           afterInput={
             gameInstallInfo?.manifest?.download_size ? (