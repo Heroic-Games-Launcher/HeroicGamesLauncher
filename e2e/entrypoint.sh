f90caa146bda81b50b6985011d197e6fdd5ee6e0
Nocccer <Nocccer@gmail.com>
2023-04-28 15:34:22 +0200

    Run e2e on windows and mac aswell

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+echo
 echo "#########"
 echo "# Build #"
 echo "#########"
 echo
 
-if [ "$TEST_PACKAGED" = "true" ]
+if [[ "$TEST_PACKAGED" == "true" ]]
 then
     yarn dist:linux
 else
     yarn vite build
 fi
 
+echo
 echo "########"
 echo "# Test #"
 echo "########"
 echo
 
-xvfb-run -a -e /dev/stdout -s "-screen 0 1280x960x24" yarn playwright test api.spec.ts
+if [[ "$OSTYPE" == *"linux"* ]]
+then
+    xvfb-run -a -e /dev/stdout -s "-screen 0 1280x960x24" yarn playwright test api.spec.ts
+else
+    yarn playwright test api.spec.ts
+fi
