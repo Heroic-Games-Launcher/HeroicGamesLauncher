fdb273ae704c24aaecb619f76ac07627622c2c52
Nocccer <Nocccer@gmail.com>
2023-04-28 15:45:18 +0200

    Fix entrypoint.sh build step

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -6,9 +6,21 @@ echo "# Build #"
 echo "#########"
 echo
 
+# get os
+os="win"
+if [[ "$OSTYPE" == *"linux"* ]]
+then
+    os="linux"
+
+elif [[ "$OSTYPE" == *"darwin"* ]]
+then
+    os="mac"
+fi
+
+# build
 if [[ "$TEST_PACKAGED" == "true" ]]
 then
-    yarn dist:linux
+    yarn dist:$os
 else
     yarn vite build
 fi
@@ -19,7 +31,7 @@ echo "# Test #"
 echo "########"
 echo
 
-if [[ "$OSTYPE" == *"linux"* ]]
+if [[ "$os" == "linux" ]]
 then
     xvfb-run -a -e /dev/stdout -s "-screen 0 1280x960x24" yarn playwright test api.spec.ts
 else
