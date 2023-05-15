3536ac488abf94895834e08c9591b7cfef57d0b8
Flavio F Lima <flavioislima@gmail.com>
2023-05-15 18:31:07 +0200

    chore: use wine-ge-proton

--- src/backend/utils.ts
+++ src/backend/utils.ts
@@ -958,7 +958,7 @@ export async function downloadDefaultWine() {
   // use Wine-GE type if on Linux and Wine-Crossover if on Mac
   const release = availableWine.filter((version) => {
     if (isLinux) {
-      return version.version.includes('Wine-GE')
+      return version.version.includes('Wine-GE-Proton')
     } else if (isMac) {
       return version.version.includes('Wine-Crossover')
     }
