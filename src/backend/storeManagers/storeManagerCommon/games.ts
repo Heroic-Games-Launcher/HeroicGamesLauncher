0c48f3337034957d9d6e45d7e0951b23923e9686
Flavio F Lima <flavioislima@gmail.com>
2023-06-06 12:35:11 +0200

    chore: set chrome on win UA for browser apps

--- src/backend/storeManagers/storeManagerCommon/games.ts
+++ src/backend/storeManagers/storeManagerCommon/games.ts
@@ -46,6 +46,8 @@ const openNewBrowserGameWindow = async (
       }
     })
 
+    browserGame.webContents.userAgent =
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
     browserGame.loadURL(browserUrl)
     browserGame.on('ready-to-show', () => browserGame.show())
 
