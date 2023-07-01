0962a2009a5a5dd7431a9fa3e6dcdd4a44940d6d
Gustavo Parreira <gustavotcparreira@gmail.com>
2023-07-01 20:32:27 +0100

    feat: launching games

--- src/backend/storeManagers/nile/games.ts
+++ src/backend/storeManagers/nile/games.ts
@@ -1,51 +1,36 @@
+import { ExecResult, ExtraInfo, GameInfo, GameSettings } from 'common/types'
+import { InstallResult } from 'common/types/game_manager'
 import {
-  ExecResult,
-  ExtraInfo,
-  GameInfo,
-  GameSettings
-  /* InstallArgs,
-  InstallPlatform */
-} from 'common/types'
-import { InstallResult /* RemoveArgs */ } from 'common/types/game_manager'
-// import { GOGCloudSavesLocation } from 'common/types/gog'
-import { getGameInfo as nileLibraryGetGameInfo } from './library'
-import { LogPrefix, logError } from 'backend/logger/logger'
+  runRunnerCommand as runNileCommand,
+  getGameInfo as nileLibraryGetGameInfo
+} from './library'
+import { LogPrefix, logError, logsDisabled } from 'backend/logger/logger'
+import { isWindows } from 'backend/constants'
+import { GameConfig } from 'backend/game_config'
+import {
+  createAbortController,
+  deleteAbortController
+} from 'backend/utils/aborthandler/aborthandler'
+import {
+  getRunnerCallWithoutCredentials,
+  launchCleanup,
+  prepareLaunch,
+  prepareWineLaunch,
+  setupEnvVars,
+  setupWrappers
+} from 'backend/launcher'
+import { appendFileSync } from 'graceful-fs'
+import { logFileLocation } from 'backend/storeManagers/storeManagerCommon/games'
+import { showDialogBoxModalAuto } from 'backend/dialog/dialog'
+import { t } from 'i18next'
+import { getWineFlags } from 'backend/utils/compatibility_layers'
+import shlex from 'shlex'
+import { join } from 'path'
+import { getNileBin } from 'backend/utils'
 
-export async function getSettings(/* appName: string */): Promise<GameSettings> {
-  // TODO: Logic
-  return {
-    autoInstallDxvk: false,
-    autoInstallVkd3d: false,
-    autoSyncSaves: false,
-    battlEyeRuntime: false,
-    DXVKFpsCap: '',
-    eacRuntime: false,
-    enableDXVKFpsLimit: false,
-    enableEsync: false,
-    enableFSR: false,
-    enableFsync: false,
-    enviromentOptions: [],
-    ignoreGameUpdates: false,
-    language: '',
-    launcherArgs: '',
-    nvidiaPrime: false,
-    offlineMode: false,
-    preferSystemLibs: false,
-    savesPath: '',
-    showFps: false,
-    showMangohud: false,
-    targetExe: '',
-    useGameMode: false,
-    useSteamRuntime: false,
-    wineCrossoverBottle: '',
-    winePrefix: '',
-    wineVersion: {
-      bin: '',
-      name: '',
-      type: 'wine'
-    },
-    wrapperOptions: []
-  }
+export async function getSettings(appName: string): Promise<GameSettings> {
+  const gameConfig = GameConfig.get(appName)
+  return gameConfig.config || (await gameConfig.getSettings())
 }
 
 export function getGameInfo(appName: string): GameInfo {
@@ -112,8 +97,8 @@ export async function install(): Promise<InstallResult> {
   }
 }
 
-export function isNative(/* appName: string */): boolean {
-  return false
+export function isNative(): boolean {
+  return isWindows
 }
 
 export async function addShortcuts(/* appName: string, fromMenu?: boolean */) {
@@ -124,10 +109,133 @@ export async function removeShortcuts(/* appName: string */) {
   return
 }
 
-export async function launch(): Promise<boolean> {
-  /* appName: string,
-  launchArguments?: string */
-  return false
+export async function launch(
+  appName: string,
+  launchArguments?: string
+): Promise<boolean> {
+  const gameSettings = await getSettings(appName)
+  const gameInfo = getGameInfo(appName)
+
+  const {
+    success: launchPrepSuccess,
+    failureReason: launchPrepFailReason,
+    rpcClient,
+    mangoHudCommand,
+    gameModeBin,
+    steamRuntime
+  } = await prepareLaunch(gameSettings, gameInfo, isNative())
+
+  if (!launchPrepSuccess) {
+    appendFileSync(
+      logFileLocation(appName),
+      `Launch aborted: ${launchPrepFailReason}`
+    )
+    showDialogBoxModalAuto({
+      title: t('box.error.launchAborted', 'Launch aborted'),
+      message: launchPrepFailReason!,
+      type: 'ERROR'
+    })
+    return false
+  }
+  const exeOverrideFlag = gameSettings.targetExe
+    ? ['--override-exe', gameSettings.targetExe]
+    : []
+
+  let commandEnv = isWindows
+    ? process.env
+    : { ...process.env, ...setupEnvVars(gameSettings) }
+  let wineFlag: string[] = []
+
+  if (!isNative()) {
+    // -> We're using Wine/Proton on Linux or CX on Mac
+    const {
+      success: wineLaunchPrepSuccess,
+      failureReason: wineLaunchPrepFailReason,
+      envVars: wineEnvVars
+    } = await prepareWineLaunch('nile', appName)
+    if (!wineLaunchPrepSuccess) {
+      appendFileSync(
+        logFileLocation(appName),
+        `Launch aborted: ${wineLaunchPrepFailReason}`
+      )
+      if (wineLaunchPrepFailReason) {
+        showDialogBoxModalAuto({
+          title: t('box.error.launchAborted', 'Launch aborted'),
+          message: wineLaunchPrepFailReason,
+          type: 'ERROR'
+        })
+      }
+      return false
+    }
+
+    commandEnv = {
+      ...commandEnv,
+      ...wineEnvVars
+    }
+
+    const { bin: wineExec, type: wineType } = gameSettings.wineVersion
+
+    // Fix for people with old config
+    const wineBin =
+      wineExec.startsWith("'") && wineExec.endsWith("'")
+        ? wineExec.replaceAll("'", '')
+        : wineExec
+
+    wineFlag = [
+      ...getWineFlags(wineBin, gameSettings, wineType),
+      '--wine-prefix',
+      gameSettings.winePrefix
+    ]
+  }
+
+  const commandParts = [
+    'launch',
+    ...exeOverrideFlag, // Check if this works
+    ...wineFlag,
+    ...shlex.split(launchArguments ?? ''),
+    ...shlex.split(gameSettings.launcherArgs ?? ''),
+    appName
+  ]
+  const wrappers = setupWrappers(
+    gameSettings,
+    mangoHudCommand,
+    gameModeBin,
+    steamRuntime?.length ? [...steamRuntime] : undefined
+  )
+
+  const fullCommand = getRunnerCallWithoutCredentials(
+    commandParts,
+    commandEnv,
+    wrappers,
+    join(...Object.values(getNileBin()))
+  )
+  appendFileSync(
+    logFileLocation(appName),
+    `Launch Command: ${fullCommand}\n\nGame Log:\n`
+  )
+
+  const { error } = await runNileCommand(
+    commandParts,
+    createAbortController(appName),
+    {
+      env: commandEnv,
+      wrappers,
+      logMessagePrefix: `Launching ${gameInfo.title}`,
+      onOutput(output) {
+        if (!logsDisabled) appendFileSync(logFileLocation(appName), output)
+      }
+    }
+  )
+
+  deleteAbortController(appName)
+
+  if (error) {
+    logError(['Error launching game:', error], LogPrefix.Nile)
+  }
+
+  launchCleanup(rpcClient)
+
+  return !error
 }
 
 export async function moveInstall(): Promise<InstallResult> {
