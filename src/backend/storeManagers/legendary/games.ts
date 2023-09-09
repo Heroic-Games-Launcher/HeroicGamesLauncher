6ccf9da0329afedd9d609f9706946e56c2add6d9
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2023-09-09 11:48:02 -0400

    [FIX] Auto enabling anti cheat runtimes

--- src/backend/storeManagers/legendary/games.ts
+++ src/backend/storeManagers/legendary/games.ts
@@ -637,12 +637,15 @@ export async function install(
   const anticheatInfo = gameAnticheatInfo(getGameInfo(appName).namespace)
 
   if (anticheatInfo && isLinux) {
-    const gameSettings = await getSettings(appName)
+    const gameConfig = GameConfig.get(appName)
 
-    gameSettings.eacRuntime =
-      anticheatInfo.anticheats.includes('Easy Anti-Cheat')
-    if (gameSettings.eacRuntime && isFlatpak) gameSettings.useGameMode = true
-    gameSettings.battlEyeRuntime = anticheatInfo.anticheats.includes('BattlEye')
+    if (anticheatInfo.anticheats.includes('Easy Anti-Cheat')) {
+      gameConfig.setSetting('eacRuntime', true)
+      if (isFlatpak) gameConfig.setSetting('useGameMode', true)
+    }
+
+    if (anticheatInfo.anticheats.includes('BattlEye'))
+      gameConfig.setSetting('battleyeRuntime', true)
   }
 
   return { status: 'done' }
