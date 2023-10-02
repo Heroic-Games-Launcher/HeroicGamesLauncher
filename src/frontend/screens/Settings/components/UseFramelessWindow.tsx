927cc4aa02de96bc5975e2efa413362b6a8cf7dd
CmdrKeen <0xcmdrkeen@pm.me>
2023-10-02 02:31:48 -0700

    Add experimental feature notification

--- src/frontend/screens/Settings/components/UseFramelessWindow.tsx
+++ src/frontend/screens/Settings/components/UseFramelessWindow.tsx
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ToggleSwitch } from 'frontend/components/UI'
 import useSetting from 'frontend/hooks/useSetting'
+import ContextProvider from 'frontend/state/ContextProvider'
 
 const UseFramelessWindow = () => {
   const { t } = useTranslation()
+  const { showDialogModal } = useContext(ContextProvider)
   const [framelessWindow, setFramelessWindow] = useSetting(
     'framelessWindow',
     false
   )
 
+  async function toggleFramelessWindow() {
+    if (!framelessWindow) {
+      showDialogModal({
+        title: t(
+          'setting.frameless-window.confirmation.title',
+          'Experimental feature ahead'
+        ),
+        message: t(
+          'setting.frameless-window.confirmation.message',
+          'This feature is still experimental. Please report any issues you encounter with it on GitHub.'
+        ),
+        buttons: [
+          {
+            text: t('box.ok'),
+            onClick: () => setFramelessWindow(true)
+          },
+          {
+            text: t('button.cancel')
+          }
+        ],
+        type: 'MESSAGE'
+      })
+      return
+    }
+    setFramelessWindow(!framelessWindow)
+  }
+
   return (
     <ToggleSwitch
       htmlId="framelessWindow"
       value={framelessWindow}
-      handleChange={() => setFramelessWindow(!framelessWindow)}
+      handleChange={toggleFramelessWindow}
       title={t(
-        'setting.frameless-window',
-        'Use frameless window (restart required)'
+        'setting.frameless-window.description',
+        'Use frameless window (requires restart)'
       )}
     />
   )
