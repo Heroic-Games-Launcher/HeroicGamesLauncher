821594e488cf7915576c22f1116766725c693e97
flegald <flegal.david@protonmail.com>
2023-10-24 14:32:22 -0700

    Adjust canceled download item button behavior

--- src/frontend/screens/DownloadManager/components/DownloadManagerItem/index.tsx
+++ src/frontend/screens/DownloadManager/components/DownloadManagerItem/index.tsx
@@ -19,6 +19,7 @@ type Props = {
   element?: DMQueueElement
   current: boolean
   state?: DownloadManagerState
+  handleClearItem?: (appName: string) => void
 }
 
 const options: Intl.DateTimeFormatOptions = {
@@ -36,7 +37,12 @@ function convertToTime(time: number) {
   }
 }
 
-const DownloadManagerItem = ({ element, current, state }: Props) => {
+const DownloadManagerItem = ({
+  element,
+  current,
+  state,
+  handleClearItem
+}: Props) => {
   const { amazon, epic, gog, showDialogModal } = useContext(ContextProvider)
   const { t } = useTranslation('gamepage')
   const { t: t2 } = useTranslation('translation')
@@ -115,8 +121,10 @@ const DownloadManagerItem = ({ element, current, state }: Props) => {
   // using one element for the different states so it doesn't
   // lose focus from the button when using a game controller
   const handleMainActionClick = () => {
-    if (finished || canceled) {
+    if (finished) {
       return goToGamePage()
+    } else if (canceled) {
+      handleClearItem && handleClearItem(appName)
     }
 
     current ? stopInstallation() : window.api.removeFromDMQueue(appName)
@@ -141,7 +149,7 @@ const DownloadManagerItem = ({ element, current, state }: Props) => {
     }
 
     if (canceled) {
-      return <DownIcon className="installIcon" />
+      return <StopIcon className="installIcon" />
     }
 
     return <StopIcon className="cancelIcon" />
