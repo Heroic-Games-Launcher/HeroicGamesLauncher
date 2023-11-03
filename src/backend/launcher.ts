c402c5d4ec8ae678c22bc96985e2db1410c5c43c
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2023-11-03 15:31:39 -0400

    small fixes

--- src/backend/launcher.ts
+++ src/backend/launcher.ts
@@ -270,12 +270,16 @@ async function prepareWineLaunch(
     }
   }
 
-  if (gameSettings.eacRuntime && !isInstalled('eac_runtime')) {
-    download('eac_runtime')
+  if (gameSettings.eacRuntime && !isInstalled('eac_runtime') && isOnline()) {
+    await download('eac_runtime')
   }
 
-  if (gameSettings.battlEyeRuntime && !isInstalled('battleye_runtime')) {
-    download('battleye_runtime')
+  if (
+    gameSettings.battlEyeRuntime &&
+    !isInstalled('battleye_runtime') &&
+    isOnline()
+  ) {
+    await download('battleye_runtime')
   }
 
   const { folder_name: installFolderName, install } =
