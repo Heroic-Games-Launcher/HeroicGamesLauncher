23ad59f4834323c27bb44e1f23290dd8459a6b97
Mathis Dröge <mathis.droege@ewe.net>
2023-11-06 18:40:49 +0100

    Move to electron-vite

---
+++ electron.vite.config.ts
@@ -0,0 +1,80 @@
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
+import { join } from 'path'
+
+import type { Plugin } from 'vite'
+
+const srcAliases = ['backend', 'frontend', 'common'].map((aliasName) => ({
+  find: aliasName,
+  replacement: join(__dirname, 'src', aliasName)
+}))
+srcAliases.push({
+  find: '~@fontsource',
+  replacement: join(__dirname, 'node_modules/@fontsource')
+})
+
+const dependenciesToNotExternalize = [
+  '@xhmikosr/decompress',
+  '@xhmikosr/decompress-targz',
+  'check-disk-space'
+]
+
+// FIXME: Potentially publish this as a dedicated plugin, if other projects
+//        run into the same issue
+const vite_plugin_react_dev_tools: Plugin = {
+  name: 'react-dev-tools-replace',
+  transformIndexHtml: {
+    handler: (html) =>
+      html.replace(
+        '<!-- REACT_DEVTOOLS_SCRIPT -->',
+        '<script src="http://localhost:8097"></script>'
+      )
+  }
+}
+
+export default defineConfig(({ mode }) => ({
+  main: {
+    build: {
+      rollupOptions: {
+        input: 'src/backend/main.ts'
+      },
+      outDir: 'build/main',
+      minify: mode === 'production',
+      sourcemap: mode === 'development' ? 'inline' : false
+    },
+    resolve: { alias: srcAliases },
+    plugins: [externalizeDepsPlugin({ exclude: dependenciesToNotExternalize })]
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        input: 'src/backend/preload.ts'
+      },
+      outDir: 'build/preload',
+      minify: mode === 'production',
+      sourcemap: mode === 'development' ? 'inline' : false
+    },
+    resolve: { alias: srcAliases },
+    plugins: [externalizeDepsPlugin({ exclude: dependenciesToNotExternalize })]
+  },
+  renderer: {
+    root: '.',
+    build: {
+      rollupOptions: {
+        input: 'index.html'
+      },
+      target: 'esnext',
+      outDir: 'build',
+      emptyOutDir: false,
+      minify: mode === 'production',
+      sourcemap: mode === 'development' ? 'inline' : false
+    },
+    resolve: { alias: srcAliases },
+    plugins: [
+      react(),
+      svgr(),
+      mode !== 'production' && vite_plugin_react_dev_tools
+    ]
+  }
+}))
