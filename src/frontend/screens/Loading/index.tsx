f2911eee6798d478fdb4954afcdc9fe1afa610f8
Paweł Lidwin <lidwinpawel@gmail.com>
2023-12-09 15:57:08 +0100

    [Tech]: electron side changes to window behavior (#3300)
    * improv: Electron side changes to window behavior
    
    window now has a background which should prevent flashing of white, window is shown after its contents are loaded
    
    * fix: use ready-to-show event apply maximize only when displaying window

--- src/frontend/screens/Loading/index.tsx
+++ src/frontend/screens/Loading/index.tsx
@@ -1,15 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import { UpdateComponentBase } from 'frontend/components/UI/UpdateComponent'
 import { useTranslation } from 'react-i18next'
 
 const Loading = function () {
   const { t } = useTranslation()
-
-  useEffect(() => {
-    window.api.loadingScreenReady()
-  })
-
   return <UpdateComponentBase message={t('label.loading', 'Loading')} />
 }
 
