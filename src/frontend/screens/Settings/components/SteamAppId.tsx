e4c3b005b34b88ca648e0c1b83568db8d278edb4
Andrew Timson <6879630+HamsterExAstris@users.noreply.github.com>
2024-01-20 22:19:39 -0500

    Add help text.

--- src/frontend/screens/Settings/components/SteamAppId.tsx
+++ src/frontend/screens/Settings/components/SteamAppId.tsx
@@ -1,4 +1,4 @@
-import { TextInputField } from 'frontend/components/UI'
+import { InfoBox, TextInputField } from 'frontend/components/UI'
 import useSetting from 'frontend/hooks/useSetting'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
@@ -8,6 +8,20 @@ const SteamAppId = () => {
 
   const [steamAppId, setSteamAppId] = useSetting('steamAppId', '')
 
+  const infobox = (
+    <InfoBox text="infobox.help">
+      {t(
+        'help.steam_appid.description',
+        'Use an alternate Steam AppId for art when adding to the Steam library.'
+      )}
+      <br />
+      {t(
+        'help.steam_appid.fallback',
+        'Leave blank to use the first AppId found in external sources (if any).'
+      )}
+    </InfoBox>
+  )
+
   return (
     <TextInputField
       label={t('setting.steam_appid', 'Override Steam AppId')}
@@ -15,6 +29,7 @@ const SteamAppId = () => {
       placeholder={t('placeholder.steam_appid', 'App Id (e.g.: 123456)')}
       value={steamAppId}
       onChange={(event) => setSteamAppId(event.target.value)}
+      afterInput={infobox}
     />
   )
 }
