48f622d6864f81d301bd6f636b1fb5285384d584
Mathis Dröge <mathis.droege@ewe.net>
2024-01-31 23:42:12 +0100

    Properly cache files downloaded with `downloadFile`

--- src/backend/utils/inet/downloader/index.ts
+++ src/backend/utils/inet/downloader/index.ts
@@ -51,8 +51,7 @@ async function downloadFile(
   if (options.maxCache && options.writeToFile) {
     const stats = await stat(options.writeToFile).catch(() => null)
     if (stats) {
-      if (stats.birthtime.valueOf() + options.maxCache * 1000 >= Date.now()) {
-        console.log('Returning cached value for', url)
+      if (stats.mtimeMs + options.maxCache * 1000 >= Date.now()) {
         switch (options.axiosConfig?.responseType) {
           case 'text':
             return readFile(options.writeToFile, 'utf-8')
