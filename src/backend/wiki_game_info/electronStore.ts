5e187b6a02a687c6a7ce1841dbe4b1da37ca78e7
Paweł Lidwin <lidwinpawel@gmail.com>
2024-02-05 22:03:28 +0100

    cache: allow custom data validity checks to be defined
    useful for use cases where we want to make sure that data won't be removed when offline etc..
    in case of ULWGL we invalidate entries that are null

--- src/backend/wiki_game_info/electronStore.ts
+++ src/backend/wiki_game_info/electronStore.ts
@@ -6,4 +6,6 @@ export const wikiGameInfoStore = new CacheStore<WikiInfo>(
   60 * 24 * 30
 )
 
-export const ulwglStore = new CacheStore<string | null>('ulwgl', 60 * 6)
+export const ulwglStore = new CacheStore<string | null>('ulwgl', 60 * 6, {
+  invalidateCheck: (data) => !data
+})
