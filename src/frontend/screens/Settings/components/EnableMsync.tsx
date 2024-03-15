97e03c7bad7ba4605f2c93d7683fe87a53121509
Maria Bonifacio <bonif.maria1@gmail.com>
2024-03-15 17:55:28 +0000

    fix_lint

--- src/frontend/screens/Settings/components/EnableMsync.tsx
+++ src/frontend/screens/Settings/components/EnableMsync.tsx
@@ -7,38 +7,36 @@ import { ToggleSwitch } from 'frontend/components/UI'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 
-
 const EnableMsync = () => {
-    const { t } = useTranslation()
-    const { platform } = useContext(ContextProvider)
-    const { isMacNative } = useContext(SettingsContext)
-    const isMac = platform === 'darwin'
-    const [enableMsync, setEnableMsync] = useSetting('enableMsync', false)
+  const { t } = useTranslation()
+  const { platform } = useContext(ContextProvider)
+  const { isMacNative } = useContext(SettingsContext)
+  const isMac = platform === 'darwin'
+  const [enableMsync, setEnableMsync] = useSetting('enableMsync', false)
 
-    if (!isMac || isMacNative){
-        return <></>
-    }
+  if (!isMac || isMacNative) {
+    return <></>
+  }
 
-    return(
-    <div className='toggleRow'>
-        <ToggleSwitch
+  return (
+    <div className="toggleRow">
+      <ToggleSwitch
         htmlId="msyncToggle"
         value={enableMsync || false}
         handleChange={() => setEnableMsync(!enableMsync)}
         title={t('setting.msync', 'Enable Msync')}
-        />
+      />
 
-        <FontAwesomeIcon
+      <FontAwesomeIcon
         className="helpIcon"
         icon={faCircleInfo}
         title={t(
-            'help.msync',
-            'Msync aims to reduce wineserver overhead in CPU-intensive games. Enabling may improve performance on supported Linux kernels.'
-          )}
-/>
-
+          'help.msync',
+          'Msync aims to reduce wineserver overhead in CPU-intensive games. Enabling may improve performance on supported Linux kernels.'
+        )}
+      />
     </div>
-    )
+  )
 }
 
 export default EnableMsync
