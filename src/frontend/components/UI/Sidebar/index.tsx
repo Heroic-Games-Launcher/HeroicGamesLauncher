a1c4516d7235639ac93dbdfd1d6e9c8398c1470d
Kajot-dev <jjaruszewski@outlook.com>
2024-04-01 22:29:37 +0200

    remove useState

--- src/frontend/components/UI/Sidebar/index.tsx
+++ src/frontend/components/UI/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import MainLinks from './components/MainLinks'
 import ExtraLinks from './components/ExtraLinks'
