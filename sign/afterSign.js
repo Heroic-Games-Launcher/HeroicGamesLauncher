a324b8b1fe4eabda3119bfd6370ef2eac1dfab3c
Flavio F Lima <flavioislima@users.noreply.github.com>
2024-04-01 11:51:08 +0100

    [CI] Use Electron-builder builtin notarize tool

--- sign/afterSign.js
+++ sign/afterSign.js
@@ -1,8 +1,8 @@
-const notarize = require("./notarize").default
+// const notarize = require("./notarize").default
 const vmp = require("./vmp").default
 
 exports.default = async function (context) {
-    await notarize(context);
+    // await notarize(context);
     console.log("Finshed Signing, signing with VMP if on Windows. Platform:", context.packager.platform.name)
     if (context.packager.platform.name === 'windows') {
         console.log("Signing with VMP on Windows")
