ca3966377f46d8442e995bf21f7cbea3ee091b07
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2024-12-16 19:23:55 +0530

    misc: fix lint

--- src/backend/utils/systeminfo/osInfo/linux.ts
+++ src/backend/utils/systeminfo/osInfo/linux.ts
@@ -45,13 +45,13 @@ async function osInfo_linux(): Promise<{ name: string; version?: string }> {
   const osName = nameMatch
     ? stripQuotes(nameMatch)
     : prettyNameMatch
-      ? stripQuotes(prettyNameMatch)
-      : 'Linux'
+    ? stripQuotes(prettyNameMatch)
+    : 'Linux'
   const osVersion = versionMatch
     ? stripQuotes(versionMatch)
     : versionId && versionCodename
-      ? `${versionId} ${versionCodename}`
-      : undefined
+    ? `${versionId} ${versionCodename}`
+    : undefined
   if (osVersion) return { name: osName, version: osVersion }
   return { name: osName }
 }
