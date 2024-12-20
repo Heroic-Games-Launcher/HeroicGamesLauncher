dcdb4dafa6f6c86f0bb7cf635ccaab7f7bba0159
Ariel Juodziukynas <arieljuod@gmail.com>
2024-12-20 00:50:35 -0300

    Emit the `launch` ipcMain event instead of sending the launchGame message to the frontend when launching with protocol

--- src/backend/protocol.ts
+++ src/backend/protocol.ts
@@ -5,6 +5,8 @@ import { getInfo } from './utils'
 import { GameInfo, Runner } from 'common/types'
 import { getMainWindow, sendFrontendMessage } from './main_window'
 import { icon } from './constants'
+import { ipcMain } from 'electron/main'
+import { gameManagerMap } from './storeManagers'
 
 type Command = 'ping' | 'launch'
 
@@ -116,6 +118,7 @@ async function handleLaunch(
   }
 
   const { is_installed, title, app_name, runner: gameRunner } = game
+  const settings = await gameManagerMap[gameRunner].getSettings(app_name)
 
   if (!is_installed) {
     logInfo(`"${title}" not installed.`, LogPrefix.ProtocolHandler)
@@ -143,7 +146,11 @@ async function handleLaunch(
   }
 
   mainWindow?.hide()
-  sendFrontendMessage('launchGame', app_name, gameRunner)
+  ipcMain.emit('launch', {
+    appName: app_name,
+    runner: gameRunner,
+    skipVersionCheck: settings.ignoreGameUpdates
+  })
 }
 
 /**
