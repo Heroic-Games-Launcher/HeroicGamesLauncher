4cdee545b009f799be7ddf9c76356701c586da7f
Ariel Juodziukynas <arieljuod@gmail.com>
2025-01-12 15:17:37 -0300

    Make install modal global with state moved to zustand

--- src/frontend/screens/Library/index.tsx
+++ src/frontend/screens/Library/index.tsx
@@ -27,7 +27,6 @@ import {
   sideloadedCategories
 } from 'frontend/helpers/library'
 import RecentlyPlayed from './components/RecentlyPlayed'
-import { InstallModal } from './components'
 import LibraryContext from './LibraryContext'
 import { Category, PlatformsFilters, StoresFilters } from 'frontend/types'
 import { hasHelp } from 'frontend/hooks/hasHelp'
@@ -35,15 +34,9 @@ import EmptyLibraryMessage from './components/EmptyLibrary'
 import CategoriesManager from './components/CategoriesManager'
 import LibraryTour from './components/LibraryTour'
 import AlphabetFilter from './components/AlphabetFilter'
+import { openInstallGameModal } from 'frontend/state/InstallGameModal'
 
 const storage = window.localStorage
-
-type ModalState = {
-  game: string
-  show: boolean
-  runner: Runner
-  gameInfo: GameInfo | null
-}
 
 export default React.memo(function Library(): JSX.Element {
   const { t } = useTranslation()
@@ -202,12 +195,6 @@ export default React.memo(function Library(): JSX.Element {
     string | null
   >(null)
 
-  const [showModal, setShowModal] = useState<ModalState>({
-    game: '',
-    show: false,
-    runner: 'legendary',
-    gameInfo: null
-  })
   const [sortDescending, setSortDescending] = useState(
     JSON.parse(storage?.getItem('sortDescending') || 'false')
   )
@@ -267,7 +254,7 @@ export default React.memo(function Library(): JSX.Element {
     runner: Runner,
     gameInfo: GameInfo | null
   ) {
-    setShowModal({ game: appName, show: true, runner, gameInfo })
+    openInstallGameModal({ appName, runner, gameInfo })
   }
 
   // cache list of games being installed
@@ -734,22 +721,6 @@ export default React.memo(function Library(): JSX.Element {
       <button id="backToTopBtn" onClick={backToTop} ref={backToTopElement}>
         <ArrowDropUp id="backToTopArrow" className="material-icons" />
       </button>
-
-      {showModal.show && (
-        <InstallModal
-          appName={showModal.game}
-          runner={showModal.runner}
-          gameInfo={showModal.gameInfo}
-          backdropClick={() =>
-            setShowModal({
-              game: '',
-              show: false,
-              runner: 'legendary',
-              gameInfo: null
-            })
-          }
-        />
-      )}
 
       {showCategories && <CategoriesManager />}
     </LibraryContext.Provider>
