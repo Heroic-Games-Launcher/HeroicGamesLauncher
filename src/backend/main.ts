47f8a2243e9b867ef0e1850d1a077ac616416502
Paweł Lidwin <lidwinpawel@gmail.com>
2025-03-03 22:20:46 +0100

    fix: use egs's flow for epic login

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -80,7 +80,6 @@ import {
   wineprefixFAQ,
   customThemesWikiLink,
   createNecessaryFolders,
-  fixAsarPath,
   isSnap,
   isWindows,
   isMac
@@ -772,9 +771,6 @@ ipcMain.handle('login', async (event, sid) => LegendaryUser.login(sid))
 ipcMain.handle('authGOG', async (event, code) => GOGUser.login(code))
 ipcMain.handle('logoutLegendary', LegendaryUser.logout)
 ipcMain.on('logoutGOG', GOGUser.logout)
-ipcMain.handle('getLocalPeloadPath', async () => {
-  return fixAsarPath(join('file://', publicDir, 'webviewPreload.js'))
-})
 
 ipcMain.handle('getAmazonLoginData', NileUser.getLoginData)
 ipcMain.handle('authAmazon', async (event, data) => NileUser.login(data))
