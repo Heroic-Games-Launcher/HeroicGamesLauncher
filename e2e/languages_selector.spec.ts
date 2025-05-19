78ec658e236bbd41cfacedeafd8b9210fb0bb88f
Ariel Juodziukynas <arieljuod@gmail.com>
2025-05-19 20:09:38 -0300

    Update test after select changes to MUI component

--- e2e/languages_selector.spec.ts
+++ e2e/languages_selector.spec.ts
@@ -4,13 +4,16 @@ import { electronTest } from './helpers'
 
 // ensure heroic always render correctly for all languages when library is empty
 electronTest('Settings', async (app, page) => {
+  test.setTimeout(120_000)
+
   for (const lang of supportedLanguages) {
     await test.step(`renders ${lang} with no errors`, async () => {
       // use selectors cause text will change for each language
       await page.locator('.Sidebar a[href="#/settings/general"]').click()
 
       await page.locator('.settingsWrapper').isVisible()
-      await page.selectOption('#languageSelector', lang)
+      await page.locator('#languageSelector').click()
+      await page.locator('li[data-value="' + lang + '"]').click()
 
       await page.locator('.Sidebar a[href="#/"]').click()
 
@@ -22,5 +25,6 @@ electronTest('Settings', async (app, page) => {
   // set english again to not break the rest of the tests
   await page.locator('.Sidebar a[href="#/settings/general"]').click()
   await page.locator('.settingsWrapper').isVisible()
-  await page.selectOption('#languageSelector', 'en')
+  await page.locator('#languageSelector').click()
+  await page.locator('li[data-value="en"]').click()
 })
