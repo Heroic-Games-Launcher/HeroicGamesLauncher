17d72327f690591c0f42cfaa1012a0497bd281aa
Ariel Juodziukynas <arieljuod@gmail.com>
2025-06-01 19:16:44 -0300

    [UX] Hide DXVK-NVAPI option on the SteamDeck

--- src/backend/constants/environment.ts
+++ src/backend/constants/environment.ts
@@ -7,6 +7,11 @@ export const isWindows = process.platform === 'win32'
 export const isLinux = process.platform === 'linux'
 export const isSteamDeckGameMode =
   process.env.XDG_CURRENT_DESKTOP === 'gamescope'
+export const isSteamDeckDesktopMode =
+  env.SESSION_MANAGER?.includes('unix/steamdeck') &&
+  env.HOME === '/home/deck' &&
+  env.DESKTOP_SESSION?.includes('steamos')
+export const isSteamDeck = isSteamDeckGameMode || isSteamDeckDesktopMode
 export const isCLIFullscreen = process.argv.includes('--fullscreen')
 export const isCLINoGui = process.argv.includes('--no-gui')
 export const isFlatpak = Boolean(env.FLATPAK_ID)
