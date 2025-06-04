35be5bd1f8b08cca26a788ba9ebad7f85430fecd
Ariel Juodziukynas <arieljuod@gmail.com>
2025-06-04 19:50:29 -0300

    Do not show "Do not use wine" in default game settings

--- src/frontend/screens/Settings/components/DoNotUseWine.tsx
+++ src/frontend/screens/Settings/components/DoNotUseWine.tsx
@@ -3,15 +3,17 @@ import { useTranslation } from 'react-i18next'
 import { ToggleSwitch } from 'frontend/components/UI'
 import useSetting from 'frontend/hooks/useSetting'
 import ContextProvider from 'frontend/state/ContextProvider'
+import SettingsContext from '../SettingsContext'
 
 const DoNotUseWine = () => {
   const { t } = useTranslation()
   const { platform } = useContext(ContextProvider)
+  const { isDefault } = useContext(SettingsContext)
   const isWindows = platform === 'win32'
 
   const [doNotUseWine, setDoNotUseWine] = useSetting('doNotUseWine', false)
 
-  if (isWindows) {
+  if (isWindows || isDefault) {
     return <></>
   }
 
