56b9b1e3577c2634d9b54efca3ce9d64a9f2fa62
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-07-12 23:34:21 +0300

    Create EnableNtsync.tsx

---
+++ src/frontend/screens/Settings/components/EnableNtsync.tsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react'
+import { useTranslation } from 'react-i18next'
+import { ToggleSwitch } from 'frontend/components/UI'
+import useSetting from 'frontend/hooks/useSetting'
+import ContextProvider from 'frontend/state/ContextProvider'
+import SettingsContext from '../SettingsContext'
+import InfoIcon from 'frontend/components/UI/InfoIcon'
+
+const EnableNtsync = () => {
+  const { t } = useTranslation()
+  const { platform } = useContext(ContextProvider)
+  const { isLinuxNative } = useContext(SettingsContext)
+  const isLinux = platform === 'linux'
+  const [enableNtsync, setEnableNtsync] = useSetting('enableNtsync', false)
+
+  if (!isLinux || isLinuxNative) {
+    return <></>
+  }
+
+  return (
+    <div className="toggleRow">
+      <ToggleSwitch
+        htmlId="NtsyncToggle"
+        value={enableNtsync || false}
+        handleChange={() => setEnableNtsync(!enableNtsync)}
+        title={t('setting.Ntsync', 'Enable Ntsync')}
+      />
+
+      <InfoIcon
+        text={t(
+          'help.Ntsync',
+          'Ntsync aims to reduce wineserver overhead in CPU-intensive games. Enabling may improve performance on supported Linux kernels.'
+        )}
+      />
+    </div>
+  )
+}
+
+export default EnableNtsync
