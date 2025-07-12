1d2073da7b4db732a643d6c2e9d084bdd1505746
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-07-12 23:26:58 +0300

    Update types.ts

--- src/common/types.ts
+++ src/common/types.ts
@@ -197,6 +197,7 @@ export interface GameSettings {
   enableFSR: boolean
   enableMsync: boolean
   enableFsync: boolean
+  enableNtsync: boolean
   enableWineWayland: boolean
   enableHDR: boolean
   gamescope: GameScopeSettings
