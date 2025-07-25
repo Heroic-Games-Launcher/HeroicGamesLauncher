e1a7890b79eead416d90029b978982317741214d
Katharina Dröge <kate@commandmc.de>
2025-07-25 02:16:13 +0200

    Change "Allow non-GE Proton" to only hide Valve Proton versions
    Instead of hiding all custom Proton versions instead of GE's, we can just not
    include Valve's versions (since those are the only ones with issues; -CachyOS,
    -Sarek and -EM all have umu support)

--- src/frontend/screens/Settings/components/ShowValveProton.tsx
+++ src/frontend/screens/Settings/components/ShowValveProton.tsx
@@ -4,38 +4,38 @@ import useSetting from 'frontend/hooks/useSetting'
 import { ToggleSwitch } from 'frontend/components/UI'
 import ContextProvider from 'frontend/state/ContextProvider'
 
-const AllowNonGEProton = () => {
+const ShowValveProton = () => {
   const { t } = useTranslation()
   const { showDialogModal } = useContext(ContextProvider)
-  const [allowNonGEProton, setAllowNonGEProton] = useSetting(
-    'allowNonGEProton',
+  const [showValveProton, setShowValveProton] = useSetting(
+    'showValveProton',
     false
   )
 
   const toggleConfig = () => {
-    if (allowNonGEProton) {
-      setAllowNonGEProton(false)
+    if (showValveProton) {
+      setShowValveProton(false)
     } else {
       showDialogModal({
         title: t(
-          'setting.allow_non_ge_proton.confirmation.title',
+          'setting.show_valve_proton.confirmation.title',
           'Are you sure?'
         ),
         message: t(
-          'setting.allow_non_ge_proton.confirmation.message',
-          'We recommend GE-Proton to be used with Heroic because non-GE Proton builds lack winetricks and protofixes. You can still use then if you want.'
+          'setting.show_valve_proton.confirmation.message',
+          "We recommend custom Proton forks (GE-Proton, Proton-CachyOS, etc.) to be used with Heroic because Valve's Proton builds lack winetricks and protofixes. You can still use then if you want."
         ),
         buttons: [
           {
             text: t(
-              'setting.allow_non_ge_proton.confirmation.show',
+              'setting.show_valve_proton.confirmation.show',
               'I understand, show them'
             ),
-            onClick: () => setAllowNonGEProton(true)
+            onClick: () => setShowValveProton(true)
           },
           {
             text: t(
-              'setting.allow_non_ge_proton.confirmation.dont_show',
+              'setting.show_valve_proton.confirmation.dont_show',
               "Don't show them"
             )
           }
@@ -48,16 +48,16 @@ const AllowNonGEProton = () => {
   return (
     <div className="toggleRow">
       <ToggleSwitch
-        htmlId="allowNonGEProton"
-        value={allowNonGEProton}
+        htmlId="showValveProton"
+        value={showValveProton}
         handleChange={() => toggleConfig()}
         title={t(
-          'setting.allow_non_ge_proton.label',
-          'Allow using non-GE Proton builds to run games'
+          'setting.show_valve_proton.label',
+          'Allow using Valve Proton builds to run games'
         )}
       />
     </div>
   )
 }
 
-export default AllowNonGEProton
+export default ShowValveProton
