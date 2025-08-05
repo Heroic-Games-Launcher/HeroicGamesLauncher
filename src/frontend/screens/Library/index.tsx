bf86cce6cb04a18c3acefcca3df00bc1b383ce1c
Ariel Juodziukynas <arieljuod@gmail.com>
2025-08-04 21:55:02 -0300

    [FIX] Back to top arrow

--- src/frontend/screens/Library/index.tsx
+++ src/frontend/screens/Library/index.tsx
@@ -238,11 +238,12 @@ export default React.memo(function Library(): JSX.Element {
   // bind back to top button
   useEffect(() => {
     if (backToTopElement.current) {
-      window.addEventListener('scroll', () => {
+      document.body.addEventListener('scroll', () => {
         const btn = document.getElementById('backToTopBtn')
         const topSpan = document.getElementById('top')
         if (btn && topSpan) {
-          btn.style.visibility = window.scrollY > 450 ? 'visible' : 'hidden'
+          btn.style.visibility =
+            document.body.scrollTop > 450 ? 'visible' : 'hidden'
         }
       })
     }
