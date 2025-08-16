0feeb9607bfcb7eeccf5206ce742c7e06c0a7779
Aidan Gauland <aidalgol@fastmail.net>
2025-08-17 08:25:34 +1200

    [Tech] Make some logging platform conditional
    Log the Steamdeck/AppImage/Flatpak messages only on Linux, to reduce
    noise in logs for macOS and Windows.

--- src/backend/utils/systeminfo/index.ts
+++ src/backend/utils/systeminfo/index.ts
@@ -122,6 +122,7 @@ async function getSystemInfo(cache = true): Promise<SystemInformation> {
 }
 
 async function formatSystemInfo(info: SystemInformation): Promise<string> {
+  const isLinux: boolean = process.platform === 'linux'
   return `CPU: ${info.CPU.cores}x ${info.CPU.model}
 Memory: ${filesize(info.memory.total)} (used: ${filesize(info.memory.used)})
 GPUs:
@@ -133,14 +134,18 @@ ${info.GPUs.map(
 ).join('\n')}
 OS: ${info.OS.name} ${info.OS.version} (${info.OS.platform})
 
-The current system is${info.steamDeckInfo.isDeck ? '' : ' not'} a Steam Deck${
-    info.steamDeckInfo.isDeck
-      ? ` (model: ${info.steamDeckInfo.model}) in ${info.steamDeckInfo.mode} mode`
-      : ''
-  }
+${
+  isLinux
+    ? `The current system is${info.steamDeckInfo.isDeck ? '' : ' not'} a Steam Deck${
+        info.steamDeckInfo.isDeck
+          ? ` (model: ${info.steamDeckInfo.model}) in ${info.steamDeckInfo.mode} mode`
+          : ''
+      }
 We are${info.isFlatpak ? '' : ' not'} running inside a Flatpak container
 We are${info.isAppImage ? '' : ' not'} running from an AppImage
-
+`
+    : ''
+}
 Software Versions:
   Heroic: ${info.softwareInUse.heroicVersion}
   Legendary: ${info.softwareInUse.legendaryVersion}
