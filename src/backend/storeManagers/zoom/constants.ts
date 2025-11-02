b52c69f46887aacd5cecab2e5bd15e7aaddb2838
neuromancer <gustavo.grieco@gmail.com>
2025-11-02 15:39:30 +0100

    pruned useless export

--- src/backend/storeManagers/zoom/constants.ts
+++ src/backend/storeManagers/zoom/constants.ts
@@ -5,4 +5,4 @@ const zoomSupportPath = join(app.getPath('userData'), 'zoom_store')
 export const embedUrl = 'https://www.zoom-platform.com'
 export const apiUrl = 'https://www.zoom-platform.com'
 export const tokenPath = join(zoomSupportPath, '.zoom.token')
-export const cachePath = join(app.getPath('userData'), 'zoom-library.json')
+const cachePath = join(app.getPath('userData'), 'zoom-library.json')
