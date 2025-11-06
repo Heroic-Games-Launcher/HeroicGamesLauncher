48f7082fcffdf9f320bc926323d7efee9ae16543
Pedro Reis <pedroreis.ad@protonmail.com>
2025-11-06 17:39:39 +0000

    add popUpOnHover, fix CSS issue

--- src/frontend/components/UI/Dropdown/index.tsx
+++ src/frontend/components/UI/Dropdown/index.tsx
@@ -6,19 +6,29 @@ type Props = {
   children: ReactNode
   className?: string
   buttonClass?: string
+  popUpOnHover?: boolean
 }
 
 export default function Dropdown({
   title,
   children,
   className,
-  buttonClass
+  buttonClass,
+  popUpOnHover = false
 }: Props) {
   const [isExpanded, setIsExpanded] = useState(false)
+
+  const handlePopup = (state: 'enter' | 'leave') => {
+    // if no pop up behavior is wanted, ignore mouse movements
+    if (!popUpOnHover) return
+    setIsExpanded(state === 'enter')
+  }
 
   return (
     <div className={`dropdownContainer ${className || ''}`}>
       <button
+        onMouseEnter={() => handlePopup('enter')}
+        onMouseLeave={() => handlePopup('leave')}
         className={`dropdownButton ${buttonClass ? buttonClass : ''}`}
         onClick={() => {
           // focus first component when expanding
@@ -32,6 +42,8 @@ export default function Dropdown({
         {title}
       </button>
       <div
+        onMouseEnter={() => handlePopup('enter')}
+        onMouseLeave={() => handlePopup('leave')}
         onBlur={() => setIsExpanded(false)}
         onFocus={() => setIsExpanded(true)}
         className={`dropdown ${isExpanded ? 'expanded' : 'collapsed'}`}
