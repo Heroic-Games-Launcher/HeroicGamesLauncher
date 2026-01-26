40b9aba0081a2133db0e6996be026aa867aec1b0
Flavio F Lima <flavioislima@users.noreply.github.com>
2026-01-26 11:42:55 +0000

    feat: Add manual cloud save synchronization options and an auto-sync toggle to the GamePage.

--- src/frontend/screens/Game/GamePage/components/CloudSavesSync.tsx
+++ src/frontend/screens/Game/GamePage/components/CloudSavesSync.tsx
@@ -1,9 +1,14 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import GameContext from '../../GameContext'
 import { CloudOff, CloudQueue } from '@mui/icons-material'
-import { GameInfo } from 'common/types'
+import { GameInfo, SyncType } from 'common/types'
+import { GOGCloudSavesLocation } from 'common/types/gog'
 import InfoIcon from 'frontend/components/UI/InfoIcon'
+import useSetting from 'frontend/hooks/useSetting'
+import { syncSaves } from 'frontend/helpers'
+import { Menu, MenuItem, Divider } from '@mui/material'
+import { ToggleSwitch } from 'frontend/components/UI'
 
 interface Props {
   gameInfo: GameInfo
@@ -11,7 +16,15 @@ interface Props {
 
 const CloudSavesSync = ({ gameInfo }: Props) => {
   const { t } = useTranslation('gamepage')
+  const { t: tCommon } = useTranslation()
   const { gameSettings, is } = useContext(GameContext)
+  const [autoSyncSaves, setAutoSyncSaves] = useSetting('autoSyncSaves', false)
+  const [savesPath] = useSetting('savesPath', '')
+  const [gogSaves] = useSetting('gogSaves', [])
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const [isSyncing, setIsSyncing] = useState(false)
+  const open = Boolean(anchorEl)
 
   if (!gameSettings) {
     return null
@@ -31,22 +44,127 @@ const CloudSavesSync = ({ gameInfo }: Props) => {
 
   const cloud_save_enabled = gameInfo.cloud_save_enabled || false
   const showCloudSaveInfo = cloud_save_enabled && !is.linuxNative
-  const { autoSyncSaves } = gameSettings
+
+  const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleSync = async (syncType: SyncType) => {
+    setIsSyncing(true)
+
+    if (gameInfo.runner === 'gog') {
+      let locations = gogSaves
+      if (!locations.length) {
+        locations = (await window.api.getDefaultSavePath(
+          gameInfo.app_name,
+          'gog',
+          []
+        )) as GOGCloudSavesLocation[]
+      }
+
+      await window.api
+        .syncGOGSaves(locations, gameInfo.app_name, syncType)
+        .then((stderr) => {
+          window.api.logError(stderr)
+        })
+    } else {
+      let path = savesPath
+      if (!path) {
+        path = (await window.api.getDefaultSavePath(
+          gameInfo.app_name,
+          'legendary',
+          []
+        )) as string
+      }
+
+      await syncSaves(path, gameInfo.app_name, gameInfo.runner, syncType)
+    }
+
+    setIsSyncing(false)
+    handleClose()
+  }
+
+  const syncCommands = [
+    { name: tCommon('setting.manualsync.download'), value: '--skip-upload' },
+    { name: tCommon('setting.manualsync.upload'), value: '--skip-download' },
+    {
+      name: tCommon('setting.manualsync.forcedownload'),
+      value: '--force-download'
+    },
+    { name: tCommon('setting.manualsync.forceupload'), value: '--force-upload' }
+  ]
 
   return (
     <>
       {showCloudSaveInfo && (
-        <p
-          style={{
-            color: autoSyncSaves ? '#07C5EF' : '',
-            margin: 0
-          }}
-          className="iconWithText"
-        >
-          <CloudQueue />
-          <b>{t('info.syncsaves')}:</b>
-          {autoSyncSaves ? t('enabled') : t('disabled')}
-        </p>
+        <>
+          <p
+            style={{
+              color: autoSyncSaves ? '#07C5EF' : '',
+              margin: 0,
+              cursor: 'pointer',
+              textDecoration: 'underline'
+            }}
+            className="iconWithText"
+            onMouseEnter={handleOpen}
+            aria-owns={open ? 'mouse-over-popover' : undefined}
+            aria-haspopup="true"
+          >
+            <CloudQueue />
+            {isSyncing ? (
+              <b>{`${tCommon('setting.manualsync.syncing')}... ${tCommon(
+                'please-wait'
+              )}`}</b>
+            ) : (
+              <>
+                <b>{t('info.syncsaves')}:</b>
+                {autoSyncSaves ? t('enabled') : t('disabled')}
+              </>
+            )}
+          </p>
+
+          <Menu
+            id="mouse-over-popover"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              onMouseLeave: handleClose,
+              style: { pointerEvents: 'auto' }
+            }}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left'
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left'
+            }}
+          >
+            {syncCommands.map((command) => (
+              <MenuItem
+                key={command.value}
+                onClick={() => handleSync(command.value as SyncType)}
+                disabled={isSyncing}
+              >
+                {command.name}
+              </MenuItem>
+            ))}
+            <Divider />
+            <MenuItem>
+              <ToggleSwitch
+                title={tCommon('setting.autosync')}
+                htmlId="autosync"
+                value={autoSyncSaves}
+                handleChange={() => setAutoSyncSaves(!autoSyncSaves)}
+              />
+            </MenuItem>
+          </Menu>
+        </>
       )}
       {!showCloudSaveInfo && (
         <p
