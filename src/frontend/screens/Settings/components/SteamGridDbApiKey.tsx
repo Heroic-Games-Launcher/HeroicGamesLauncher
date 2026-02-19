4c4a44a398c3848fb6be54143b5ae32733299dba
Flavio F Lima <flavioislima@users.noreply.github.com>
2026-02-19 17:40:05 +0000

    feat: add help description for SteamGridDB API key in settings

--- src/frontend/screens/Settings/components/SteamGridDbApiKey.tsx
+++ src/frontend/screens/Settings/components/SteamGridDbApiKey.tsx
@@ -18,14 +18,17 @@ export default function SteamGridDbApiKey() {
         onChange={(newValue) => setSetting('steamGridDbApiKey', newValue)}
         value={config.steamGridDbApiKey || ''}
         htmlId="steamgriddb-api-key"
+        afterInput={
+          <InfoBox text={t('settings.advanced.details', 'Details')}>
+            <span style={{ userSelect: 'text' }}>
+              {t(
+                'settings.steamgriddb.help.description',
+                'Provide your own SteamGridDB API key to enable game cover search. You can get one at www.steamgriddb.com/profile/preferences/api'
+              )}
+            </span>
+          </InfoBox>
+        }
       />
-
-      <InfoBox text={t('settings.advanced.details', 'Details')}>
-        {t(
-          'settings.steamgriddb.apikey.description',
-          'Provide your own SteamGridDB API key to enable cover search. You can get one at www.steamgriddb.com/profile/preferences/api'
-        )}
-      </InfoBox>
     </div>
   )
 }
