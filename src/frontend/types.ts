9ea06693c61b2b33dffd8f8410713197a258ed50
Ariel Juodziukynas <arieljuod@gmail.com>
2026-02-20 08:40:37 -0300

    [UX] Fix incorrect loading state in Wine Manager when library was refreshed (#5293)

--- src/frontend/types.ts
+++ src/frontend/types.ts
@@ -41,7 +41,6 @@ export interface ContextType {
   isIntelMac: boolean
   refresh: (library: Runner, checkUpdates?: boolean) => Promise<void>
   refreshLibrary: (options: RefreshOptions) => Promise<void>
-  refreshWineVersionInfo: (fetch: boolean) => void
   refreshing: boolean
   refreshingInTheBackground: boolean
   hiddenGames: {
