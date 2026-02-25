85d3fc186b0d7ce45d6e7f59b31fb3ce2e028804
Katharina Dröge <kate@commandmc.de>
2026-02-25 17:08:58 +0100

    Flip error check condition to remove one level of indent

--- src/backend/utils.ts
+++ src/backend/utils.ts
@@ -293,55 +293,55 @@ async function errorHandler({
   // this message appears on macOS when no Crossover was found in the system but its a false alarm
   const ignoreCrossoverMessage = 'IndexError: list index out of range'
 
-  if (error) {
-    if (error.includes(ignoreCrossoverMessage)) {
+  if (!error) return
+
+  if (error.includes(ignoreCrossoverMessage)) {
+    return
+  }
+  if (error.includes(deletedFolderMsg) && appName) {
+    const runner = r.toLocaleLowerCase() as Runner
+    const { title } = gameManagerMap[runner].getGameInfo(appName)
+    const { response } = await showMessageBox({
+      type: 'question',
+      title,
+      message: i18next.t(
+        'box.error.folder-not-found.title',
+        'Game folder appears to be deleted, do you want to remove the game from the installed list?'
+      ),
+      buttons: [i18next.t('box.no'), i18next.t('box.yes')]
+    })
+
+    if (response === 1) {
+      return gameManagerMap[runner].forceUninstall(appName)
+    }
+  }
+
+  if (legendaryRegex.test(error)) {
+    const MemoryError = 'MemoryError: '
+    if (error.includes(MemoryError)) {
       return
     }
-    if (error.includes(deletedFolderMsg) && appName) {
-      const runner = r.toLocaleLowerCase() as Runner
-      const { title } = gameManagerMap[runner].getGameInfo(appName)
-      const { response } = await showMessageBox({
-        type: 'question',
-        title,
-        message: i18next.t(
-          'box.error.folder-not-found.title',
-          'Game folder appears to be deleted, do you want to remove the game from the installed list?'
-        ),
-        buttons: [i18next.t('box.no'), i18next.t('box.yes')]
-      })
 
-      if (response === 1) {
-        return gameManagerMap[runner].forceUninstall(appName)
-      }
-    }
+    return showDialogBoxModalAuto({
+      title: plat,
+      message: i18next.t(
+        'box.error.legendary.generic',
+        'An error has occurred! Try to Logout and Login on your Epic account. {{newline}}  {{error}}',
+        { error, newline: '\n' }
+      ),
+      type: 'ERROR'
+    })
+  }
 
-    if (legendaryRegex.test(error)) {
-      const MemoryError = 'MemoryError: '
-      if (error.includes(MemoryError)) {
-        return
-      }
-
-      return showDialogBoxModalAuto({
-        title: plat,
-        message: i18next.t(
-          'box.error.legendary.generic',
-          'An error has occurred! Try to Logout and Login on your Epic account. {{newline}}  {{error}}',
-          { error, newline: '\n' }
-        ),
-        type: 'ERROR'
-      })
-    }
-
-    if (error.includes(expiredCredentials)) {
-      return showDialogBoxModalAuto({
-        title: plat,
-        message: i18next.t(
-          'box.error.credentials.message',
-          'Your Crendentials have expired, Logout and Login Again!'
-        ),
-        type: 'ERROR'
-      })
-    }
+  if (error.includes(expiredCredentials)) {
+    return showDialogBoxModalAuto({
+      title: plat,
+      message: i18next.t(
+        'box.error.credentials.message',
+        'Your Crendentials have expired, Logout and Login Again!'
+      ),
+      type: 'ERROR'
+    })
   }
 }
 
