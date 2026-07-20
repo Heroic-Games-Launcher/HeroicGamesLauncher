a26adc389a48928655eaffffd6c0446094a05f91
Katharina Dröge <kate@commandmc.de>
2026-07-20 18:07:19 +0200

    Fixup import side effect in setup, consolidate setup and ipc_handler

--- src/backend/exe_handler/ipc_handler.ts
+++
@@ -1,7 +0,0 @@
-import { addHandler } from '../ipc'
-
-import { launchExe } from '.'
-
-addHandler('exe_handler.launchWithExeFile', (_e, exePath, appName, runner) =>
-  launchExe(exePath, appName, runner)
-)
--- src/backend/main.ts
+++ src/backend/main.ts
@@ -142,7 +142,6 @@ import {
 } from './constants/paths'
 import { supportedLanguages } from 'common/languages'
 import MigrationSystem from './migration'
-import './exe_handler/setup'
 
 if (isLinux) app.commandLine?.appendSwitch('--gtk-version', '3')
 
@@ -1453,4 +1452,4 @@ import './recent_games/ipc_handler'
 import './tools/ipc_handler'
 import './progress_bar'
 import './steamgrid/ipc_handler'
-import './exe_handler/ipc_handler'
+import './exe_handler/setup'
