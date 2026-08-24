48a7e35cd5034943a4aa2ac7a41c1ed7d6951acd
Flavio F Lima <flavioislima@users.noreply.github.com>
2026-08-24 15:03:30 +0100

    fix: sign windows binaries during build via SignPath

---
+++ sign/signpath.js
@@ -0,0 +1,65 @@
+const { execFileSync } = require('child_process')
+const { renameSync, rmSync } = require('fs')
+
+const ORGANIZATION_ID = 'f7f99393-5adb-408c-8ca3-67e07cfe31e6'
+const PROJECT_SLUG = 'HeroicGamesLauncher'
+const SIGNING_POLICY_SLUG = 'release-signing'
+
+exports.default = async function sign(configuration) {
+  if (configuration.hash && configuration.hash.toLowerCase() !== 'sha256') {
+    return
+  }
+
+  if (!process.env.SIGNPATH_API_TOKEN) {
+    console.warn(
+      `SIGNPATH_API_TOKEN not set, skipping signing of ${configuration.path}`
+    )
+    return
+  }
+
+  if (configuration.path.includes('app.asar.unpacked')) {
+    return
+  }
+
+  const outputPath = `${configuration.path}.signed`
+
+  const commandParts = [
+    "$ErrorActionPreference = 'Stop'",
+    'Submit-SigningRequest' +
+      ' -InputArtifactPath $env:SIGNPATH_INPUT' +
+      ' -ApiToken $env:SIGNPATH_API_TOKEN' +
+      ` -OrganizationId '${ORGANIZATION_ID}'` +
+      ` -ProjectSlug '${PROJECT_SLUG}'` +
+      ` -SigningPolicySlug '${SIGNING_POLICY_SLUG}'` +
+      (process.env.SIGNPATH_ARTIFACT_CONFIGURATION
+        ? ' -ArtifactConfigurationSlug $env:SIGNPATH_ARTIFACT_CONFIGURATION'
+        : '') +
+      ' -OutputArtifactPath $env:SIGNPATH_OUTPUT' +
+      ' -WaitForCompletion'
+  ]
+
+  console.log(`Submitting ${configuration.path} to SignPath`)
+
+  execFileSync(
+    'powershell.exe',
+    [
+      '-NoProfile',
+      '-NonInteractive',
+      '-ExecutionPolicy',
+      'Bypass',
+      '-Command',
+      commandParts.join('; ')
+    ],
+    {
+      stdio: 'inherit',
+      env: {
+        ...process.env,
+        SIGNPATH_INPUT: configuration.path,
+        SIGNPATH_OUTPUT: outputPath
+      }
+    }
+  )
+
+  rmSync(configuration.path)
+  renameSync(outputPath, configuration.path)
+}
