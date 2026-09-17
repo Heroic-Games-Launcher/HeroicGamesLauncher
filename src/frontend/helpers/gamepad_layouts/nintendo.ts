4eaf2b91a8f871f23c1fdf3dc5d5a970b944c9a5
Snugglefox <snugglevixen@proton.me>
2026-09-18 05:28:56 +1200

    Update nintendo.ts

--- src/frontend/helpers/gamepad_layouts/nintendo.ts
+++ src/frontend/helpers/gamepad_layouts/nintendo.ts
@@ -112,7 +112,7 @@ export function checkWiiU(
     down = buttons[14],
     left = buttons[15],
     right = buttons[16],
-    guideButton = buttons[12],
+    guideButton = buttons[10],
     leftAxisX = axes[0],
     leftAxisY = axes[1],
     rightAxisX = axes[2],
