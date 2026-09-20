80ec5e9b6978adb06e2f63e1a8b7f7705ed5e7f4
Katharina Dröge <kate@commandmc.de>
2026-09-20 14:06:14 +0200

    fix: Make sure to only pass fixed-identity getters to `useAwaited`
    The reasoning behind this is explained in the new JSDoc comment on `useAwaited`

--- src/frontend/hooks/useAwaited.ts
+++ src/frontend/hooks/useAwaited.ts
@@ -6,6 +6,22 @@ import {
   type SetStateAction
 } from 'react'
 
+/**
+ * Asynchronously invokes `getter` with `args` and returns either its return
+ * value, or `null` (if the response is not there yet)
+ *
+ * NOTE: Make sure that `getter` has a fixed identity (that it is not a
+ *       dynamically computed function expression (`() => { ... }`)). Either
+ *       pass in a direct reference (e.g. `window.api.foo` instead of
+ *       `() => window.api.foo()`) or memoize it first
+ *       (`useCallback(() => { ... })`).
+ *       If `getter` is not fixed, using this hook will cause infinite
+ *       re-renders (`getter` returning a value triggers a re-render, which
+ *       changes `getter`'s identity, which means it's called again and returns
+ *       a new value... repeat).
+ * @param getter The async function to invoke (potentially with `args`)
+ * @param args The arguments to pass to `getter`
+ */
 export function useAwaited<T, Args extends unknown[]>(
   getter: (...args: Args) => Promise<T>,
   ...args: Args
